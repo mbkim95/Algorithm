4ca7a0a2fdb78a0476b9ed2b6582336f628d2276
mbkim95 <mbkim95@naver.com>
2018-12-23 01:40:05 +0900

    Algorithm
    Implementation

--- Algospot/JOSEPHUS.cpp
+++ Algospot/JOSEPHUS.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
 #include <list>
+#include <algorithm>
 using namespace std;
 
 int main() {
-	int c;
+	int c, n, k;
 	cin >> c;
 	while (c--) {
-		int n, k;
 		cin >> n >> k;
 		list<int> l;
-
-		for (int i = 1; i <= n; i++) {
+		
+		for (int i = 1; i <= n; i++)
 			l.push_back(i);
-		}
-		
+
 		auto it = l.begin();
-		
 		while (n > 2) {
 			it = l.erase(it);
+			n--;
 			if (it == l.end())
 				it = l.begin();
-			n--;
-			for (int i = 0; i < k - 1; i++) {
+
+			int cnt = (k - 1) % n;
+
+			for (int i = 0; i < cnt; i++) {
 				it++;
 				if (it == l.end())
 					it = l.begin();
-			}
+			}	
 		}
+
 		cout << l.front() << ' ' << l.back() << '\n';
 	}
 	return 0;
