5c1292a126cf23dcd9d37c9a7b1d89ddfac332b4
mbkim95 <mbkim95@naver.com>
2019-08-01 18:50:41 +0900

    Implementation

--- Baekjoon/1927.cpp
+++ Baekjoon/1927.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 using namespace std;
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	int n;
 	cin >> n;
-	priority_queue<int, vector<int>, greater<int>> q;
+	priority_queue<int> q;
 	for (int i = 0; i < n; i++) {
 		int tmp;
 		cin >> tmp;
 		if (tmp == 0) {
 			if (q.empty())
-				cout << 0 << '\n';
+				cout << "0\n";
 			else {
-				cout << q.top() << '\n';
+				tmp = -q.top();
 				q.pop();
+				cout << tmp << '\n';
 			}
 		}
-		else
-			q.push(tmp);
+		else {
+			q.push(-tmp);
+		}
 	}
 	return 0;
 }
