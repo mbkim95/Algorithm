bc4aaf8cc461b75cb4ccf19a28a6118826b12ca0
mbkim95 <mbkim95@naver.com>
2019-08-01 18:50:31 +0900

    DFS

--- Baekjoon/1520.cpp
+++ Baekjoon/1520.cpp
@@ -3,35 +3,45 @@
 using namespace std;
 
 int n, m;
-int board[500][500], chk[500][500];
-int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, -1, 0, 1};
+int board[501][501], chk[501][501];
+const int dx[] = { -1, 0, 1, 0 }, dy[] = { 0, -1, 0, 1 };
 
-int dfs(int x, int y){
-	if(chk[x][y] != -1)
-		return chk[x][y];
-
-	if(x>=n || y>=m || x < 0 || y < 0)
-		return 0;
-	
-	if(x == 0 && y == 0)
-		return 1;
-	
-	chk[x][y] = 0;
-
-	for(int i=0; i<4; i++){	
-		if(board[x][y] < board[x+dx[i]][y+dy[i]]){
-			chk[x][y] += dfs(x+dx[i], y+dy[i]);
-		}
-	}
-	return chk[x][y];
+bool inRange(int x, int y) {
+	return (0 <= x && x < m) && (0 <= y && y < n);
 }
 
-int main(){
+int dfs(int x, int y) {
+	int& ret = chk[y][x];
+
+	if (ret != -1)
+		return chk[y][x];
+
+	if (!inRange(x, y))
+		return 0;
+
+	if (x == 0 && y == 0)
+		return 1;
+
+	ret = 0;
+
+	for (int i = 0; i<4; i++) {
+		if (board[y][x] < board[y + dy[i]][x + dx[i]]) {
+			ret += dfs(x + dx[i], y + dy[i]);
+		}
+	}
+	return ret;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	cin >> n >> m;
-	for(int i=0; i<n; i++)
-		for(int j=0; j<m; j++)
+	for (int i = 0; i<n; i++)
+		for (int j = 0; j<m; j++)
 			cin >> board[i][j];
 
 	memset(chk, -1, sizeof(chk));
-	cout << dfs(n-1, m-1) << endl;
+	cout << dfs(m - 1, n - 1) << endl;
 }
