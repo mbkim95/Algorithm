a237db01282effc2a28ee6af6bf15698f2e0a1a9
mbkim95 <mbkim95@naver.com>
2020-04-02 20:31:02 +0900

    Implementation

---
+++ Programmers/주식가격.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    for(int i=0; i<prices.size(); i++){
+        int cnt=0;
+        for(int j=i+1; j<prices.size(); j++){
+            cnt++;
+            if(prices[i] > prices[j]) break;
+        }
+        answer.push_back(cnt);
+    }
+    return answer;
+}
