376ec29c8a6d6761134022f81fbd9f7350b8b142
mbkim95 <mbkim95@naver.com>
2020-04-07 16:21:28 +0900

    Dynamic Programming

---
+++ Programmers/자연수 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long dp[2001];
+
+long long solve(int idx) {
+	if (dp[idx] != -1) return dp[idx];
+	return dp[idx] = (solve(idx - 1) + solve(idx - 2)) % 1234567;
+}
+
+long long solution(int n) {
+	long long answer = 0;
+	fill(dp, dp + n + 1, -1);
+	dp[1] = 1;
+	dp[2] = 2;
+	answer = solve(n);
+	return answer;
+}
