25c94447c6d25a72f8e5660d6e2ea53a678e5096
mbkim95 <mbkim95@naver.com>
2020-04-11 21:01:50 +0900

    Implementation

--- Baekjoon/2455.cpp
+++ Baekjoon/2455.cpp
@@ -3,19 +3,18 @@
 #include <algorithm>
 using namespace std;
 
-vector<pair<int, int>>v;
-int ans;
-
 int main() {
-	for (int i = 0; i < 4; i++) {
-		int tmp1, tmp2;
-		cin >> tmp1 >> tmp2;
-		v.push_back(make_pair(tmp1, tmp2));
-	}
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 
-	ans = max(ans, v[0].second);
-	ans = max(ans, ans - v[1].first + v[1].second);
-	ans = max(ans, ans - v[2].first + v[2].second);
+	int ans = 0;
+	int cur = 0;
+	for (int i = 0; i < 4; i++) {
+		int out, in;
+		cin >> out >> in;
+		cur += (in - out);
+		ans = max(ans, cur);
+	}
 	cout << ans << '\n';
 	return 0;
 }
