9b1d1092a9f2089aade954dde854bc3ef626a945
mbkim95 <mbkim95@naver.com>
2020-04-14 21:07:19 +0900

    Binary Search

--- Programmers/예산.cpp
+++ Programmers/예산.cpp
@@ -1,18 +1,28 @@
-#include <iostream>
-#include <stdio.h>
 #include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-int solution(vector<int> d, int budget) {
-    int answer = 0;
-    sort(d.begin(), d.end());
-    int sum = 0;
-    for(int i=0; i<d.size(); i++){
-        if(sum + d[i] > budget) break;
-        sum += d[i];
-        answer++;
-    }
-    return answer;
+int solution(vector<int> budgets, int M) {
+	int answer = 0;
+	int left = 0;
+    //
+	int right = *max_element(budgets.begin(), budgets.end());
+	int sum = 0;
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		sum = 0;
+		for (int v : budgets){
+			if (v < mid) sum += v;
+			else sum += mid;
+		}
+		if (sum > M) {
+			right = mid - 1;
+		}
+		else {
+			answer = max(answer, mid);
+			left = mid + 1;
+		}
+	}
+	return answer;
 }
