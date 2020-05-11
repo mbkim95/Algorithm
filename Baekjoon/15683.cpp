5aeb64fc9bfd7a2f4f3947609572756352d06866
mbkim95 <mbkim95@naver.com>
2020-05-11 16:24:33 +0900

    Simulation

--- Baekjoon/15683.cpp
+++ Baekjoon/15683.cpp
@@ -4,362 +4,72 @@
 using namespace std;
 
 int n, m, ans;
-vector<vector<int>> map;
-vector<vector<bool>> visited;
+vector<vector<int>> map, tmp;
 vector<pair<int, int>> cctv;
-vector<int> direction;
+int cctv_dir[8];
+const int dx[] = { 1, 0, -1, 0 }, dy[] = { 0, 1, 0, -1 };
 
-bool inRange(int x, int y) {
-	return (0 <= x && x < m) && (0 <= y && y < n);
-}
+void observe(int x, int y, int dir) {
+	int nx = x; int ny = y;
 
-void cctv1(vector<vector<int>>& map, int idx) {
-	const int dx[] = { 1, 0, -1, 0 }, dy[] = { 0, 1, 0, -1 };
-	int x = cctv[idx].first;
-	int y = cctv[idx].second;
-	int dir = direction[idx];
-	
-	while (inRange(x, y) && (map[y][x] != 6)) {
-		if (!map[y][x])
-			map[y][x] = 9;
-		x += dx[dir];
-		y += dy[dir];
+	while (true) {
+		nx += dx[dir]; ny += dy[dir];
+		if (nx < 0 || nx > m - 1 || ny < 0 || ny > n - 1 || tmp[ny][nx] == 6) break;
+		if (tmp[ny][nx] != 0) continue;
+		tmp[ny][nx] = -1;
 	}
 }
 
-void cctv2(vector<vector<int>>& map, int idx) {
-	const int dx[] = { 1, 0, -1, 0 }, dy[] = { 0, 1, 0, -1 };
-	int x = cctv[idx].first;
-	int y = cctv[idx].second;
-	int dir = direction[idx];
-
-	if (dir == 1 || dir == 3) {
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[0];
-			y += dy[0];
-		}
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[2];
-			y += dy[2];
-		}
-	}
-	else if (dir == 2 || dir == 4) {
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[1];
-			y += dy[1];
-		}
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[3];
-			y += dy[3];
-		}
-	}
-}
-
-void cctv3(vector<vector<int>>& map, int idx) {
-	const int dx[] = { 1, 0, -1, 0 }, dy[] = { 0, 1, 0, -1 };
-	int x = cctv[idx].first;
-	int y = cctv[idx].second;
-	int dir = direction[idx];
-
-	if (dir == 1) {
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[0];
-			y += dy[0];
-		}
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[1];
-			y += dy[1];
-		}
-	}
-	else if (dir == 2) {
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[1];
-			y += dy[1];
-		}
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[2];
-			y += dy[2];
-		}
-	}
-	else if (dir == 3) {
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[2];
-			y += dy[2];
-		}
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[3];
-			y += dy[3];
-		}
-	}
-	else {
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[3];
-			y += dy[3];
-		}
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[0];
-			y += dy[0];
-		}
-	}
-}
-
-void cctv4(vector<vector<int>>& map, int idx) {
-	const int dx[] = { 1, 0, -1, 0 }, dy[] = { 0, 1, 0, -1 };
-	int x = cctv[idx].first;
-	int y = cctv[idx].second;
-	int dir = direction[idx];
-
-	if (dir == 1) {
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[0];
-			y += dy[0];
-		}
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[1];
-			y += dy[1];
-		}
-
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[2];
-			y += dy[2];
-		}
-	}
-	else if (dir == 2) {
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[1];
-			y += dy[1];
-		}
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[2];
-			y += dy[2];
-		}
-
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[3];
-			y += dy[3];
-		}
-	}
-	else if (dir == 3) {
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[2];
-			y += dy[2];
-		}
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[3];
-			y += dy[3];
-		}
-
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[0];
-			y += dy[0];
-		}
-	}
-	else {
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[3];
-			y += dy[3];
-		}
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[0];
-			y += dy[0];
-		}
-
-		x = cctv[idx].first;
-		y = cctv[idx].second;
-
-		while (inRange(x, y) && (map[y][x] != 6)) {
-			if (!map[y][x])
-				map[y][x] = 9;
-			x += dx[1];
-			y += dy[1];
-		}
-	}
-}
-
-void cctv5(vector<vector<int>>& map, int idx) {
-	const int dx[] = { 1, 0, -1, 0 }, dy[] = { 0, 1, 0, -1 };
-	int x = cctv[idx].first;
-	int y = cctv[idx].second;
-	int dir = direction[idx];
-
-	while (inRange(x, y) && (map[y][x] != 6)) {
-		if (!map[y][x])
-			map[y][x] = 9;
-		x += dx[0];
-		y += dy[0];
-	}
-
-	x = cctv[idx].first;
-	y = cctv[idx].second;
-
-	while (inRange(x, y) && (map[y][x] != 6)) {
-		if (!map[y][x])
-			map[y][x] = 9;
-		x += dx[1];
-		y += dy[1];
-	}
-
-	x = cctv[idx].first;
-	y = cctv[idx].second;
-
-	while (inRange(x, y) && (map[y][x] != 6)) {
-		if (!map[y][x])
-			map[y][x] = 9;
-		x += dx[2];
-		y += dy[2];
-	}
-
-	x = cctv[idx].first;
-	y = cctv[idx].second;
-
-	while (inRange(x, y) && (map[y][x] != 6)) {
-		if (!map[y][x])
-			map[y][x] = 9;
-		x += dx[3];
-		y += dy[3];
-	}
-}
-
-int getAnswer(vector<vector<int>>& map) {
+int simulation() {
+	tmp = map;
 	int ret = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (map[i][j] == 0)
-				ret++;
-		}
+
+	for (int i = 0; i < cctv.size(); i++) {
+		int x = cctv[i].first; int y = cctv[i].second;
+		int model = map[y][x];
+		observe(x, y, cctv_dir[i]);
+		if (model == 2 || model == 4 || model == 5) observe(x, y, (cctv_dir[i] + 2) % 4);
+		if (model == 3 || model == 4 || model == 5) observe(x, y, (cctv_dir[i] + 1) % 4);
+		if (model == 5) observe(x, y, cctv_dir[i] + 3);
 	}
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			if (!tmp[i][j]) ret++;
 	return ret;
 }
 
-void dfs(int idx) {
-	if (idx == cctv.size()) {
-		vector<vector<int>> tmp = map;
-		for (int i = 0; i < idx; i++) {
-			int x = cctv[i].first;
-			int y = cctv[i].second;
-			int id = map[y][x];
-			if (id == 1)
-				cctv1(tmp, i);
-			else if (id == 2) 
-				cctv2(tmp, i);
-			else if(id ==3)
-				cctv3(tmp, i);
-			else if(id ==4)
-				cctv4(tmp, i);
-			else
-				cctv5(tmp, i);
-		}
-		ans = min(ans, getAnswer(tmp));
+void dfs(int cnt) {
+	if (cnt == cctv.size()) {
+		ans = min(ans, simulation());
 		return;
 	}
 
+	int x = cctv[cnt].first;
+	int y = cctv[cnt].second;
+	int& tv = map[y][x];
+
 	for (int i = 0; i < 4; i++) {
-		direction[idx] = i;
-		dfs(idx + 1);
+		if(tv == 2 && (i == 2 || i == 3)) continue;
+		if (tv == 5 && i > 0) continue;
+		cctv_dir[cnt] = i;
+		dfs(cnt + 1);
 	}
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
 	cin >> n >> m;
+	ans = m * n;
 	map = vector<vector<int>>(n, vector<int>(m));
-	visited = vector<vector<bool>>(n, vector<bool>(m, false));
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) 
 		for (int j = 0; j < m; j++) {
 			cin >> map[i][j];
 			if (map[i][j] != 0 && map[i][j] != 6)
-				cctv.push_back(make_pair(j, i));
+				cctv.push_back({ j, i });
 		}
-	}
-	direction = vector<int>(cctv.size());
-	ans = 987654321;
 	dfs(0);
 	cout << ans << '\n';
 	return 0;
