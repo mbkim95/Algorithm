4d5a7030a08a151fe46cf50494923d9924ba2926
mbkim95 <mbkim95@naver.com>
2020-06-19 21:15:23 +0900

    Implementation

---
+++ Baekjoon/15655.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n, m;
+int num[8];
+vector<int> ans;
+
+void dfs(int idx, int cnt) {
+	if (cnt == m) {
+		for (int i : ans)
+			cout << i << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = idx + 1; i < n; i++) {		
+		ans.push_back(num[i]);
+		dfs(i, cnt + 1);
+		ans.pop_back();
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> num[i];
+	sort(num, num + n);
+	dfs(-1, 0);
+	return 0;
+}
