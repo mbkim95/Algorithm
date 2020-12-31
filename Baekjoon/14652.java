ae750cdc79ce3c4003a6ee9e86f17ef989c2b084
김민범 <44355413+mbkim95@users.noreply.github.com>
2020-12-31 15:48:43 +0900

    Math

---
+++ Baekjoon/14652.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n, m, k;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        bw.write(k / m + " " + k % m);
+        bw.flush();
+    }
+}
