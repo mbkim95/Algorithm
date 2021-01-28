543795e8b9a1b1cb85c995efa0a37a189c1ae5af
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-01-28 20:57:08 +0900

    Math

---
+++ Baekjoon/1085.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  int x, y, w, h;
+  cin >> x >> y >> w >> h;
+  cout << min(min(x, w - x), min(y, h - y)) << '\n';
+  return 0;
+}
