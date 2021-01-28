ae18ee16b14813abecfe7037a7ba594ba474426d
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-01-29 02:13:46 +0900

    Implementation

--- Baekjoon/2447.cpp
+++ Baekjoon/2447.cpp
@@ -13,38 +13,24 @@ void print() {
 	}
 }
 
-void drawStar(int x, int y, int size) {
+void draw_star(int x, int y, int size) {
 	if (size == 1) {
 		star[y][x] = '*';
 		return;
 	}
-	drawStar(x + size / 3 * 0, y + size / 3 * 0, size / 3);
-	drawStar(x + size / 3 * 1, y + size / 3 * 0, size / 3);
-	drawStar(x + size / 3 * 2, y + size / 3 * 0, size / 3);
-	drawStar(x + size / 3 * 0, y + size / 3 * 1, size / 3);
-	drawStar(x + size / 3 * 2, y + size / 3 * 1, size / 3);
-	drawStar(x + size / 3 * 0, y + size / 3 * 2, size / 3);
-	drawStar(x + size / 3 * 1, y + size / 3 * 2, size / 3);
-	drawStar(x + size / 3 * 2, y + size / 3 * 2, size / 3);
-}
-
-void solve(int x, int y, int size) {
-	drawStar(x + size / 3 * 0, y + size / 3 * 0, size / 3);
-	drawStar(x + size / 3 * 1, y + size / 3 * 0, size / 3);
-	drawStar(x + size / 3 * 2, y + size / 3 * 0, size / 3);
-	drawStar(x + size / 3 * 0, y + size / 3 * 1, size / 3);
-	drawStar(x + size / 3 * 2, y + size / 3 * 1, size / 3);
-	drawStar(x + size / 3 * 0, y + size / 3 * 2, size / 3);
-	drawStar(x + size / 3 * 1, y + size / 3 * 2, size / 3);
-	drawStar(x + size / 3 * 2, y + size / 3 * 2, size / 3);
+	draw_star(x + size / 3 * 0, y + size / 3 * 0, size / 3);
+	draw_star(x + size / 3 * 1, y + size / 3 * 0, size / 3);
+	draw_star(x + size / 3 * 2, y + size / 3 * 0, size / 3);
+	draw_star(x + size / 3 * 0, y + size / 3 * 1, size / 3);
+	draw_star(x + size / 3 * 2, y + size / 3 * 1, size / 3);
+	draw_star(x + size / 3 * 0, y + size / 3 * 2, size / 3);
+	draw_star(x + size / 3 * 1, y + size / 3 * 2, size / 3);
+	draw_star(x + size / 3 * 2, y + size / 3 * 2, size / 3);
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
 	cin >> n;
 	star = vector<vector<char>>(n, vector<char>(n, ' '));
-	solve(0, 0, n);
+	draw_star(0, 0, n);
 	print();
 }
