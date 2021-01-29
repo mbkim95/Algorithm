d54e70fbf208b583cecaf9b4984b71a0f5745186
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-01-29 22:51:59 +0900

    Brute Force

--- Baekjoon/2798.cpp
+++ Baekjoon/2798.cpp
@@ -1,48 +1,23 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
-int n, m, ans;
-vector<int> card;
-
-void dfs(int idx, int cnt, int sum) {
-	if (sum > m)
-		return;
-
-	if (idx == n)
-		return;
-
-	if (cnt == 3) {
-		if (sum == m){
-			cout << m << '\n';
-			exit(0);
-		}
-		else if (sum < m)
-			ans = max(ans, sum);
-		return;
-	}
-
-	for (int i = idx + 1; i < n; i++)
-		dfs(i, cnt + 1, sum + card[i]);
-}
-
-void solve() {
-	for (int i = 0; i < n - 2; i++)
-		dfs(i, 1, card[i]);
-	cout << ans << '\n';
-}
+int n, m, ans, card[300000];
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> n >> m;
-	card = vector<int>(n);
-	for (int i = 0; i < n; i++) {
-		cin >> card[i];
-	}
-	sort(card.begin(), card.end());
-	solve();
-	return 0;
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) cin >> card[i];
+  for (int i = 0; i < n - 2; i++)
+    for (int j = i + 1; j < n - 1; j++)
+      for (int k = j + 1; k < n; k++) {
+        if (card[i] + card[j] + card[k] == m) {
+          ans = m;
+          break;
+        }
+        if (card[i] + card[j] + card[k] < m) {
+          ans = max(ans, card[i] + card[j] + card[k]);
+        }
+      }
+  cout << ans << '\n';
+  return 0;
 }
