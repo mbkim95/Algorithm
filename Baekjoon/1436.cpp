0b2a7b0928d6cb66894092cd909435864e4b5c59
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-01-30 18:35:35 +0900

    Brute Force

--- Baekjoon/1436.cpp
+++ Baekjoon/1436.cpp
@@ -1,37 +1,16 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int n;
-vector<int> num;
-
-bool is666(int num) {
-	int chk = 0;
-	int tmp;
-	while (num > 0) {
-		tmp = num % 10;
-		num /= 10;
-		if (tmp == 6)
-			chk++;
-		else
-			chk = 0;
-		if (chk == 3)
-			return true;
-	}
-	return false;
-}
-
-void precalc() {
-	num.push_back(0);
-	for (int i = 666; i < 2666800; i++) {
-		if (is666(i))
-			num.push_back(i);
-	}
-}
-
 int main() {
-	cin >> n;
-	precalc();
-	cout << num[n] << '\n';
-	return 0;
+  int n, cnt = 0;
+  cin >> n;
+
+  int s = 665;
+  while(cnt < n){
+	  s++;
+	  string num = to_string(s);
+	  if(num.find("666") != string::npos) cnt++;
+  }
+  cout << s << '\n';
+  return 0;
 }
