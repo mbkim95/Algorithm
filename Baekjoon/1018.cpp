bbe87f36c03247d53375a769179354f9f97b0a49
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-01-30 18:30:54 +0900

    Brute Force

--- Baekjoon/1018.cpp
+++ Baekjoon/1018.cpp
@@ -2,79 +2,46 @@
 #include <algorithm>
 using namespace std;
 
-int n, m;
-char b1[8][9], b2[51][51];
+char board[50][50];
 
-int countRow(char base, int col) {        // 한 행의 잘못된 색깔의 개수를 셈
-	int cnt = 0;
-
-	for (int i = 0; i < 8; i++) {
-		if (base != b1[col][i])
-			cnt++;
-
-		if (base == 'W')
-			base = 'B';
-		else
-			base = 'W';
-	}
-	return cnt;
-}
-
-int countAll(char start) {        // 모든 칸의 잘못된 색깔의 개수를 셈
-	int ret = 0;
-
-	for (int i = 0; i < 8; i++) {
-		ret += countRow(start, i);
-		if (start == 'W')
-			start = 'B';
-		else
-			start = 'W';
-	}
-	return ret;
-}
-
-void cutBoard(int startCol, int startRow) {        // 8x8의 체스판으로 자르는 함수
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			b1[i][j] = b2[i + startCol][j + startRow];
-		}
-	}
-}
-
-void print() {
-	cout << '\n';
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			cout << b1[i][j];
-		}
-		cout << '\n';
-	}
-	cout << '\n';
-}
-
-int solve() {
-	int ret = 64;
-
-	int a = n - 8, b = m - 8;
-
-	for (int i = 0; i <= a; i++) {
-		for (int j = 0; j <= b; j++) {
-			cutBoard(i, j);
-			//	print();
-			ret = min(min(ret, countAll('W')), countAll('B'));
-		}
-	}
-	return ret;
+int calculate(char cut[8][8]) {
+  int ans1 = 0, ans2 = 0;
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      char tmp = cut[i][j];
+      if (!(i % 2) && !(j % 2)) {
+        if (tmp != 'W') ans1++;
+		else ans2++;
+      } else if (!(i % 2) && j % 2) {
+        if (tmp != 'B') ans1++;
+        else ans2++;
+      } else if (i % 2 && !(j % 2)) {
+        if (tmp != 'B') ans1++;
+        else ans2++;
+      } else {
+        if (tmp != 'W') ans1++;
+        else ans2++;
+      }
+    }
+  }
+  return min(ans1, ans2);
 }
 
 int main() {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> b2[i][j];
-		}
-	}
-	cout << solve() << '\n';
+  int n, m;
+  cin >> n >> m;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++) cin >> board[i][j];
 
-	return 0;
+  int ans = 987654321;
+  for (int i = 0; i <= n - 8; i++) {
+    for (int j = 0; j <= m - 8; j++) {
+      char tmp[8][8];
+      for (int k = 0; k < 8; k++)
+        for (int l = 0; l < 8; l++) tmp[k][l] = board[i + k][j + l];
+      ans = min(ans, calculate(tmp));
+    }
+  }
+  cout << ans << '\n';
+  return 0;
 }
