d1296568e797e5a2fa7ce7ecd6e67195fdbd6be1
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-01-31 10:40:37 +0900

    Implementation

--- Baekjoon/1427.cpp
+++ Baekjoon/1427.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
-int main() {
-	string str;
-	cin >> str;
-	vector<char> v;
-	for (int i = 0; i < str.size(); i++) {
-		char tmp = str.at(i);
-		v.push_back(tmp);
-	}
-	sort(v.begin(), v.end(), greater<int>());
-	
-	for (auto x : v)
-		cout << x;
-	cout << '\n';
+bool comp(char& a, char& b){
+  return a > b;
+}
+
+int main(){
+  string n;
+  cin >> n;
+  sort(n.begin(), n.end(), comp);
+  cout << n << '\n';
+  return 0;
 }
