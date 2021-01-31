dfc58b9cedd687d2faef6cb6255fec668e596590
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-01-31 10:49:20 +0900

    Implementation

--- Baekjoon/1181.cpp
+++ Baekjoon/1181.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <algorithm>
+#include <set>
 using namespace std;
 
-int main() {
-	int n;
-	cin >> n;
-	vector<string> v(n);
-	for (int i = 0; i < n; i++)
-		cin >> v[i];
+bool cmp(string& a, string& b){
+  if(a.length() == b.length()) return a < b;
+  return a.length() < b.length();
+}
 
-	sort(v.begin(), v.end(), [](string a, string b) {
-		return make_pair(a.size(), a) < make_pair(b.size(), b);
-	});
-
-	v.erase(unique(v.begin(), v.end()), v.end());
-
-	for (auto x : v)
-		cout << x << '\n';
-	return 0;
+int main(){
+  int n;
+  cin >> n;
+  vector<string> w;
+  set<string> s;
+  for(int i=0; i<n; i++) {
+    string tmp;
+    cin >> tmp;
+    if(s.find(tmp) != s.end()) continue;
+    s.insert(tmp);
+    w.push_back(tmp);
+  }
+  sort(w.begin(), w.end(), cmp);
+  for(auto i : w) cout << i << '\n';
+  return 0;
 }
