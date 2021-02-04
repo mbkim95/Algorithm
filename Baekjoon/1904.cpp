087ef90385a1cebf2e87ff5bc0d28273e78f7bd5
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-04 22:53:58 +0900

    Dynamic Programming

--- Baekjoon/1904.cpp
+++ Baekjoon/1904.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
-#define Div 15746;
 using namespace std;
 
-long dp[1000001];
+int dp[1000001];
 
 int main() {
-	int n;
-	cin >> n;
-	
-	dp[0] = 0;
-	dp[1] = 1;
-	dp[2] = 2;
-	
-	for (int i = 3; i <= n; i++)
-		dp[i] = (dp[i - 1] + dp[i - 2]) % Div;
-
-	cout << dp[n] << endl;
+  int n;
+  cin >> n;
+  dp[1] = 1;
+  dp[2] = 2;
+  for (int i = 3; i <= n; i++) dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+  cout << dp[n] << '\n';
 }
