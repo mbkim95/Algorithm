739280a6c03f03828f750da5ece7c362a9c09c19
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-05 20:36:56 +0900

    Dynamic Programming

--- Baekjoon/1149.cpp
+++ Baekjoon/1149.cpp
@@ -1,38 +1,22 @@
 #include <iostream>
-#include <cstring>
 #include <algorithm>
 using namespace std;
 
-int n, h[1000][3], dp[1000][3];
-
-int solve(int cnt, int idx) {
-	if (cnt == n)
-		return 0;	
-
-	int& ret = dp[cnt][idx];
-
-	if (ret != -1)
-		return ret;
-
-	ret = 987654321;
-
-	for (int i = 0; i < 3; i++)
-		if (idx != i)
-			ret = min(ret, h[cnt][idx] + solve(cnt + 1, i));
-
-	return ret;
-}
+int n, h[1001][3], dp[1001][3];
 
 int main() {
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < 3; j++)
-			cin >> h[i][j];
-	}
-	memset(dp, -1, sizeof(dp));
-	int ans = 987654321;
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> h[i][0] >> h[i][1] >> h[i][2];
+  dp[0][0] = h[0][0];
+  dp[0][1] = h[0][1];
+  dp[0][2] = h[0][2];
 
-	for (int i = 0; i < 3; i++)
-		ans = min(ans, solve(0, i));
-	cout << ans << '\n';	
+  for(int i=1; i<n; i++){
+      dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + h[i][0];
+      dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + h[i][1];
+      dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + h[i][2];
+  }
+  int ans = min(min(dp[n-1][0], dp[n-1][1]), dp[n-1][2]);
+  cout << ans << '\n';
+  return 0;
 }
