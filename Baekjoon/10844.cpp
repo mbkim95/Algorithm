327cd322164a5db9c45e9915db809337b0b49fb7
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-06 02:42:55 +0900

    Dynamic Programming

--- Baekjoon/10844.cpp
+++ Baekjoon/10844.cpp
@@ -1,36 +1,21 @@
 #include <iostream>
-#include <algorithm>
+#define MOD 1000000000
 using namespace std;
 
-#define DEV 1000000000
-int n, dp[101][10];
-
-void precalc() {
-	for (int i = 1; i < 10; i++)
-		dp[1][i] = 1;
-}
-
-long long solve() {	
-	for (int i = 2; i <= n; i++) {
-		for (int j = 0; j < 10; j++) {
-			if (j == 0)
-				dp[i][j] = dp[i - 1][j + 1] % DEV;
-			else if (j == 9)
-				dp[i][j] = dp[i - 1][j - 1] % DEV;
-			else
-				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % DEV;
-		}		
-	}
-
-	long long sum = 0;
-	for (int i = 0; i < 10; i++)
-		sum += dp[n][i];
-
-	return sum % DEV;
-}
+int dp[101][10];
 
 int main() {
-	cin >> n;
-	precalc();
-	cout << solve() << '\n';
+  int n;
+  cin >> n;
+  for (int i = 1; i < 10; i++) dp[1][i] = 1;
+  for (int i = 2; i <= n; i++) {
+    dp[i][0] = dp[i - 1][1];
+    for (int j = 1; j < 9; j++)
+      dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+    dp[i][9] = dp[i - 1][8];
+  }
+  int ans = 0;
+  for (int i = 0; i < 10; i++) ans = (ans + dp[n][i]) % MOD;
+  cout << ans << '\n';
+  return 0;
 }
