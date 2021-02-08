c909a377218518bf05c3ec3110745dba0c8ae794
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-08 13:39:21 +0900

    Dynamic Progarmming

--- Baekjoon/11054.cpp
+++ Baekjoon/11054.cpp
@@ -1,58 +1,28 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
-int n;
-vector<int> num, decr, incr;
-
-int calcDecrease(int idx) {
-	int& ret = decr[idx];
-
-	if (ret != -1)
-		return ret;
-
-	ret = 1;
-	
-	for (int i = idx+ 1; i < n; i++) {
-		if (num[idx] > num[i])
-			ret = max(ret, 1 + calcDecrease(i));
-	}
-	return ret;
-}
-
-int calcIncrease(int idx) {
-	int& ret = incr[idx];
-
-	if (ret != -1)
-		return ret;
-
-	ret = 1;
-
-	for (int i = idx - 1; i >= 0; i--) {
-		if (num[idx] > num[i])
-			ret = max(ret, 1 + calcIncrease(i));
-	}
-	return ret;
-}
+int n, num[1001], dp[1001], dp2[1001];
 
 int main() {
-	cin >> n;
-	num = vector<int>(n);
-	decr = vector<int>(n, -1);
-	incr = vector<int>(n, -1);
-	for (int i = 0; i < n; i++)
-		cin >> num[i];
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> num[i];
+  dp[0] = 1;
+  dp2[n - 1] = 1;
+  for (int i = 1; i < n; i++) {
+    dp[i] = 1;
+    for (int j = 0; j < i; j++)
+      if (num[i] > num[j] && dp[i] < dp[j] + 1) dp[i] = dp[j] + 1;
+  }
 
-	for (int i = 0; i < n; i++) {
-		calcDecrease(i);
-		calcIncrease(i);
-	}
+  for (int i = n - 2; i >= 0; i--) {
+    dp2[i] = 1;
+    for (int j = n - 1; j > i; j--)
+      if (num[i] > num[j] && dp2[i] < dp2[j] + 1) dp2[i] = dp2[j] + 1;
+  }
 
-	int ans = 0;
-	for (int i = 0; i < n; i++) {
-		ans = max(ans, incr[i] + decr[i] - 1);
-	}
-	cout << ans << '\n';
-	return 0;
+  int ans = 0;
+  for (int i = 0; i < n; i++) ans = max(ans, dp[i] + dp2[i] - 1);
+  cout << ans << '\n';
+  return 0;
 }
