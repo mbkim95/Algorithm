1640b52fc144693266656729a279caef21cca334
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-15 20:24:33 +0900

    Greedy

--- Baekjoon/11047.cpp
+++ Baekjoon/11047.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int n, k, num[11];
+
 int main() {
-	int coin[10];
-	int n, k, ans = 0;
+  cin >> n >> k;
+  int ans = 0;
+  for (int i = 0; i < n; i++) cin >> num[i];
 
-	cin >> n >> k;
-	for (int i = 0; i < n; i++)
-		cin >> coin[i];
-
-	int i = n - 1;
-
-	while (k != 0) {
-		if (k / coin[i] > 0) {
-			ans += (k / coin[i]);
-			k -= (coin[i] * (k / coin[i]));
-		}
-		i--;
-	}
-	cout << ans << endl;
+  while (k) {
+    for (int i = n - 1; i >= 0; i--) {
+      if (k / num[i] > 0) {
+        ans += k / num[i];
+        k %= num[i];
+        break;
+      }
+    }
+  }
+  cout << ans << '\n';
+  return 0;
 }
