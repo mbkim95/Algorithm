74d36deeef66fda0d53ccdccf66a38146e1320ff
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-16 20:35:23 +0900

    Dynamic Programming

--- Baekjoon/9251.cpp
+++ Baekjoon/9251.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
 using namespace std;
 
-string str1, str2;
+string a, b;
 int dp[1001][1001];
 
-int LCS() {
-	int n = str1.length(), m = str2.length();
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			if (str1[i - 1] == str2[j - 1])
-				dp[i][j] = dp[i - 1][j - 1] + 1;
-			else
-				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-		}
-	}
-	return dp[n][m];
-}
-
 int main() {
-	cin >> str1;
-	cin >> str2;
-	cout << LCS() << '\n';
-	return 0;
+  cin >> a >> b;
+  int ans = 0;
+  for (int i = 0; i < a.length(); i++) {
+    for (int j = 0; j < b.length(); j++) {
+      if (a[i] == b[j]) dp[i + 1][j + 1] = dp[i][j] + 1;
+      else dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1]);
+      ans = max(ans, dp[i+1][j+1]);
+    }
+  }
+  cout << ans << '\n';
+  return 0;
 }
