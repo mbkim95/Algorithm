c3eaaa0a8788e7a4936c8421ab8b77d3b2d1e612
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-17 21:49:54 +0900

    Dynamic Programming

--- Baekjoon/12865.cpp
+++ Baekjoon/12865.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
+int n, k;
+int w[101], v[101], dp[101][100001];
+
 int main() {
-	int n, k;
-	cin >> n >> k;
-	vector<int> w = vector<int>(n + 1);
-	vector<int> v = vector<int>(n + 1);
-	vector<vector<int>> dp = vector<vector<int>>(n + 1, vector<int>(k + 1, 0));
+  cin >> n >> k;
+  for (int i = 1; i <= n; i++)
+    cin >> w[i] >> v[i];
 
-	for (int i = 1; i <= n; i++) 
-		cin >> w[i] >> v[i];
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
-			if (w[i] > j)
-				dp[i][j] = dp[i - 1][j];
-			else
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
-		}
-	}
-
-	cout << dp[n][k] << '\n';
-	return 0;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= k; j++) {
+      dp[i][j] = dp[i - 1][j];
+      if (j >= w[i])
+        dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i]);
+    }
+  }
+  cout << dp[n][k] << '\n';
+  return 0;
 }
