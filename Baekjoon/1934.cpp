c3e0f1844c4ec27a094a609439b3ba31d0bdba07
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-19 22:15:08 +0900

    Math

--- Baekjoon/1934.cpp
+++ Baekjoon/1934.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int getGCD(int a, int b) {
-	if (b == 0)
-		return a;
-	return getGCD(b, a % b);
+int gcd(int a, int b) {
+  if (a % b == 0) return b;
+  return gcd(b, a % b);
 }
 
 int main() {
-	int t;
-	cin >> t;
-
-	while (t--) {
-		int a, b;
-		cin >> a >> b;
-		int gcd = a >= b ? getGCD(a, b) : getGCD(b, a);
-		cout << a * b / gcd << '\n';
-	}
-	return 0;
+  int t;
+  cin >> t;
+  while (t--) {
+    int a, b;
+    cin >> a >> b;
+    int g = gcd(a, b);
+    cout << (a / g) * (b / g) * g << '\n';
+  }
+  return 0;
 }
