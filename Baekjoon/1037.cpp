90857ab70cacae369fc2df8aa966eb799af83b41
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-19 22:12:38 +0900

    Math

--- Baekjoon/1037.cpp
+++ Baekjoon/1037.cpp
@@ -1,27 +1,13 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
 #include <algorithm>
 using namespace std;
 
-vector<int> num;
+int n, num[51];
 
 int main() {
-	int n;
-	cin >> n;
-	num = vector<int>(n);
-	for (int i = 0; i < n; i++)
-		cin >> num[i];
-	sort(num.begin(), num.end());
-	int ans;
-	if (n % 2) {
-		int mid = (n - 1) / 2;
-		ans = pow(num[mid], 2);
-	}
-	else {
-		int mid = (n - 1) / 2;
-		ans = num[mid] * num[mid + 1];
-	}
-	cout << ans << '\n';
-	return 0;
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> num[i];
+  sort(num, num + n);
+  cout << num[0] * num[n - 1] << '\n';
+  return 0;
 }
