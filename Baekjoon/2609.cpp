ffb331648c89c6299a7a61164787104bfe2561ab
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-19 22:13:01 +0900

    Math

--- Baekjoon/2609.cpp
+++ Baekjoon/2609.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int a, b;
-	cin >> a >> b;
+int gcd(int a, int b) {
+  if (a % b == 0) return b;
+  return gcd(b, a % b);
+}
 
-	int gcd = 1;
-	int div = 2;
-	while (div <= a && div <= b) {
-		if (a % div == 0 && b % div == 0) {
-			gcd *= div;
-			a /= div;
-			b /= div;
-		}
-		else {
-			div++;
-		}
-	}
-	cout << gcd << '\n' << a * b * gcd << '\n';
-	return 0;
+int main() {
+  int a, b;
+  cin >> a >> b;
+  int g = gcd(a, b);
+  cout << g << '\n' << (a / g) * (b / g) * g << '\n';
+  return 0;
 }
