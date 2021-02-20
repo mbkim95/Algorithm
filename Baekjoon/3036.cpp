989bb3cf6337d6a5d067ef8b1f9984fe11b2df01
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-20 19:15:48 +0900

    Math

--- Baekjoon/3036.cpp
+++ Baekjoon/3036.cpp
@@ -1,32 +1,18 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int n, root;
-vector<int> wheels;
-
-int getGCD(int num1, int num2) {
-	if (num2 == 0) {
-		return num1;
-	}
-	return getGCD(num2, num1 % num2);
+int gcd(int a, int b) {
+  if (a % b == 0) return b;
+  return gcd(b, a % b);
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> n;
-	cin >> root;
-	wheels = vector<int>(n - 1);
-	for (int i = 0; i < n - 1; i++) {
-		cin >> wheels[i];
-	}
-
-	for (int i = 0; i < n - 1; i++) {
-		int gcd = getGCD(root, wheels[i]);
-		cout << root / gcd << '/' << wheels[i] / gcd << '\n';
-	}
-	return 0;
+  int n, r, tmp;
+  cin >> n >> r;
+  for (int i = 1; i < n; i++) {
+    cin >> tmp;
+    int g = gcd(r, tmp);
+    cout << r / g << '/' << tmp / g << '\n';
+  }
+  return 0;
 }
