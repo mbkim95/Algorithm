72342e45de30264a44546c64c124536058abfe92
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-22 21:53:09 +0900

    Math

--- Baekjoon/9375.cpp
+++ Baekjoon/9375.cpp
@@ -3,26 +3,22 @@
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+  int t, n;
+  cin >> t;
 
-	int n;
-	int t;
-	cin >> t;
-	while (t--) {
-		map<string, int> clothes;
-		cin >> n;
-		for (int i = 0; i < n; i++) {
-			string str1, str2;
-			cin >> str1 >> str2;
-			clothes[str2]++;
-		}
-
-		int ans = 1;
-		for (auto it : clothes) {
-			ans *= (it.second + 1);
-		}
-		cout << ans - 1 << '\n';
-	}
+  while (t--) {
+    cin >> n;
+    map<string, int> m;
+    for (int i = 0; i < n; i++) {
+      string s1, s2;
+      cin >> s1 >> s2;
+      m[s2]++;
+    }
+    int ans = 1;
+    for (auto it : m) {
+      ans *= (it.second + 1);
+    }
+    cout << ans - 1 << '\n';
+  }
+  return 0;
 }
