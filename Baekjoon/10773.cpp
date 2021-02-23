8a7c587e0bfcf3c53df2c0afbc3ee2724794fd51
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-24 01:31:41 +0900

    Implementation

--- Baekjoon/10773.cpp
+++ Baekjoon/10773.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int k;
-	vector<int>money;
-	cin >> k;
-	for (int i = 0; i < k; i++) {
-		int tmp;
-		cin >> tmp;
-		if (tmp != 0)
-			money.push_back(tmp);
-		else
-			money.pop_back();
-	}
-	long long ans = 0;
-	for (int i = 0; i < money.size(); i++)
-		ans += money[i];
-	cout << ans << '\n';
-	return 0;
+  int k, tmp, ans = 0;
+  stack<int> st;
+  cin >> k;
+  while (k--) {
+    cin >> tmp;
+    if (tmp == 0) st.pop();
+    if (tmp != 0) st.push(tmp);
+  }
+  while(st.size() > 0){
+    ans += st.top();
+    st.pop();
+  }
+  cout << ans << '\n';
+  return 0;
 }
