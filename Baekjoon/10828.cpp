e1e694c9c7888da8fcb9d7240401ee2759d5e9db
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-24 01:24:16 +0900

    Implementation

--- Baekjoon/10828.cpp
+++ Baekjoon/10828.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
 #include <stack>
-#include <string>
 using namespace std;
 
 int main() {
-	int n;
-	ios_base::sync_with_stdio(false);
-	cin >> n;
-	stack<int> s;
-	while (n--) {
-		string str;
-		cin >> str;
+  int n;
+  stack<int> st;
+  cin >> n;
 
-		if (str == "push") {
-			int tmp;
-			cin >> tmp;
-			s.push(tmp);
-		}
-		else if (str == "pop") {
-			if (s.empty()) {				
-				cout << -1 << '\n';
-			}else {
-				cout << s.top() << '\n';
-				s.pop();
-			}
-		}
-		else if (str == "size") {
-			cout << s.size() << '\n';
-		}
-		else if (str == "empty") {
-			cout << s.empty() << '\n';
-		}
-		else if (str == "top") {
-			if (s.empty())
-				cout << -1 << '\n';
-			else
-				cout << s.top() << '\n';
-		}
-	}
+  while (n--) {
+    int value;
+    string cmd;
+    cin >> cmd;
+    if (cmd == "push") {
+      cin >> value;
+      st.push(value);
+    } else if (cmd == "pop") {
+      if (st.empty())
+        cout << "-1\n";
+      else {
+        cout << st.top() << '\n';
+        st.pop();
+      }
+    } else if (cmd == "size") {
+      cout << st.size() << '\n';
+    } else if (cmd == "empty") {
+      if (st.empty())
+        cout << "1\n";
+      else
+        cout << "0\n";
+    } else if (cmd == "top") {
+      if (st.empty())
+        cout << "-1\n";
+      else
+        cout << st.top() << '\n';
+    }
+  }
+  return 0;
 }
