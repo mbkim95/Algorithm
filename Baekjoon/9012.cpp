e7bd23aea654c6183d2c8be7c2c1e88bc9b23621
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-02-25 20:59:45 +0900

    Stack

--- Baekjoon/9012.cpp
+++ Baekjoon/9012.cpp
@@ -1,38 +1,28 @@
 #include <iostream>
-#include <string>
 #include <stack>
 using namespace std;
 
-int main() {
-	int c;
-	cin >> c;
-	stack<char> s;
-	while (c--) {
-		string vps;
-		cin >> vps;
-		int flag = 0;
-		for (int i = 0; i < vps.size(); i++) {
-			s.push(vps.at(i));
-		}
-		int a = 0, b = 0;
-		for (int i = 0; i < vps.size(); i++) {
-			char tmp = s.top();
-			s.pop();
-			if (tmp == '(')
-				a++;
-			else if (tmp == ')')
-				b++;
-			if (a > b) {
-				cout << "NO" << '\n';
-				flag = 1;
-				break;
-			}
-		}
-		if (flag == 0) {
-			if (a != b)
-				cout << "NO" << '\n';
-			else
-				cout << "YES" << '\n';
-		}
-	}
+int main(){
+  int t;
+  cin >> t;
+  while(t--){
+   string str;
+   stack<char> st;
+   cin >> str;
+
+   bool find = true;
+   for(char c : str){
+     if(c == '(') st.push('(');
+     else{
+       if(st.empty()) {
+         find = false;
+         break;
+       }
+       st.pop();
+     }
+   } 
+   if(!st.empty() || !find) cout << "NO\n";
+   else cout << "YES\n";
+  }
+  return 0;
 }
