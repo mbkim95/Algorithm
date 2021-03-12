8ecab224822edf1922c76980ceae71fd55d14ee9
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-03-12 21:58:47 +0900

    Queue

--- Baekjoon/10866.cpp
+++ Baekjoon/10866.cpp
@@ -1,58 +1,49 @@
 #include <iostream>
 #include <deque>
-#include <string>
 using namespace std;
 
- 	int n;
-	cin >> n;
-	deque<int> d;
-
-	while (n--) {
-		string s;
-		cin >> s;
-		if (s == "push_back") {
-			int tmp;
-			cin >> tmp;
-			d.push_back(tmp);
-		}
-		else if (s == "push_front") {
-			int tmp;
-			cin >> tmp;
-			d.push_front(tmp);
-		}
-		else if (s == "pop_front") {
-			if (d.empty())
-				cout << -1 << '\n';
-			else {
-				cout << d.front() << '\n';
-				d.pop_front();
-			}
-		}
-		else if (s == "pop_back") {
-			if (d.empty())
-				cout << -1 << '\n';
-			else {
-				cout << d.back() << '\n';
-				d.pop_back();
-			}
-		}
-		else if (s == "size") {
-			cout << d.size() << '\n';
-		}
-		else if (s == "empty") {
-			cout << d.empty() << '\n';
-		}
-		else if (s == "front") {
-			if (d.empty())
-				cout << -1 << '\n';
-			else
-				cout << d.front() << '\n';
-		}
-		else if (s == "back") {
-			if (d.empty())
-				cout << -1 << '\n';
-			else
-				cout << d.back() << '\n';
-		}
-	}
+int main() {
+  int n, tmp;
+  deque<int> dq;
+  cin >> n;
+  string cmd;
+  for (int i = 0; i < n; i++) {
+    cin >> cmd;
+    if (cmd == "push_front") {
+      cin >> tmp;
+      dq.push_front(tmp);
+    } else if (cmd == "push_back") {
+      cin >> tmp;
+      dq.push_back(tmp);
+    } else if (cmd == "pop_front") {
+      if (dq.empty())
+        cout << "-1\n";
+      else {
+        cout << dq.front() << '\n';
+        dq.pop_front();
+      }
+    } else if (cmd == "pop_back") {
+      if (dq.empty())
+        cout << "-1\n";
+      else {
+        cout << dq.back() << '\n';
+        dq.pop_back();
+      }
+    } else if (cmd == "size") {
+      cout << dq.size() << '\n';
+    } else if (cmd == "empty") {
+      cout << dq.empty() << '\n';
+    } else if (cmd == "front") {
+      if (dq.empty())
+        cout << "-1\n";
+      else
+        cout << dq.front() << '\n';
+    } else if (cmd == "back") {
+      if (dq.empty())
+        cout << "-1\n";
+      else
+        cout << dq.back() << '\n';
+    }
+  }
+  return 0;
 }
