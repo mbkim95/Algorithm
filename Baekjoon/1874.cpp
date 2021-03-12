3b8a073be6e750b075fb8591ccfb34e971aea57d
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-03-13 01:16:05 +0900

    Stack

--- Baekjoon/1874.cpp
+++ Baekjoon/1874.cpp
@@ -3,51 +3,27 @@
 #include <vector>
 using namespace std;
 
+int n;
+stack<int> st;
+vector<char> v;
+
 int main() {
-	int n;
-	cin >> n;
-	stack<int> s;
-	vector<int> num = vector<int>(n + 1);
-	vector<char> result;
-	bool possible = true;
-
-	for (int i = 0; i < n; i++)
-		cin >> num[i];
-
-	int cnt = 1;
-	for (int i = 0; i < n; i++) {
-		if (num[i] >= cnt) {
-			while (num[i] != cnt) {
-				result.push_back('+');
-				s.push(cnt);
-				cnt++;
-			}
-			result.push_back('+');
-			result.push_back('-');
-			cnt++;
-		}
-		else {
-			while (!s.empty() && num[i] != s.top()) {
-				result.push_back('-');
-				s.pop();
-			}
-
-			if (s.empty()) {
-				possible = false;
-				break;
-			}
-			result.push_back('-');
-			s.pop();
-		}
-	}
-
-	if (possible) {
-		for (int i = 0; i < result.size(); i++) {
-			cout << result[i] << '\n';
-		}
-	}
-	else {
-		cout << "NO\n";
-	}
-	return 0;
+  bool is_possible = true;
+  cin >> n;
+  int cur = 1;
+  for (int i = 0; i < n; i++) {
+    int tmp;
+    cin >> tmp;
+    while (tmp >= cur) {
+      st.push(cur++);
+      v.push_back('+');
+    }
+    if (!st.empty() && st.top() == tmp) {
+      st.pop();
+      v.push_back('-');
+    } else is_possible = false;
+  }
+  if (is_possible) for (char c : v) cout << c << '\n';
+  else cout << "NO\n";
+  return 0;
 }
