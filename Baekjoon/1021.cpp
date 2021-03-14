f4b18d7ffa3aa5bb0e24ce7c30061bc4bdbd3f4b
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-03-14 23:00:38 +0900

    Deque

--- Baekjoon/1021.cpp
+++ Baekjoon/1021.cpp
@@ -1,46 +1,36 @@
 #include <iostream>
-#include <deque>
 #include <algorithm>
+#include <deque>
 using namespace std;
 
 int n, m, ans;
 deque<int> dq;
 
 int main() {
-	cin >> n >> m;
+  cin >> n >> m;
+  for(int i=1; i<=n; i++) dq.push_back(i);
 
-	for (int i = 1; i <= n; i++)
-		dq.push_back(i);
-
-	for (int i = 0; i < m; i++) {
-		int num;
-		cin >> num;
-
-		int idx;
-		for (int j = 0; j < dq.size(); j++) {
-			if (num == dq[j]) {
-				idx = j;
-				break;
-			}
-		}
-		if (idx > dq.size() - idx) {
-			for (int j = 0; j < dq.size() - idx; j++) {
-				int tmp = dq.back();
-				dq.pop_back();
-				dq.push_front(tmp);
-			}
-			ans += dq.size() - idx;
-		}
-		else {
-			for (int i = 0; i < idx; i++) {
-				int tmp = dq.front();
-				dq.pop_front();
-				dq.push_back(tmp);
-			}
-			ans += idx;
-		}
-		dq.pop_front();
-	}
-	cout << ans << '\n';
-	return 0;
+  for (int i = 0; i < m; i++) {
+    int tmp;
+    cin >> tmp;
+    int left_move = 0, right_move = 0;
+    while (dq.front() != tmp) {
+      dq.push_back(dq.front());
+      dq.pop_front();
+      left_move++;
+    }
+    for (int i = 0; i < left_move; i++) {
+      dq.push_front(dq.back());
+      dq.pop_back();
+    }
+    while (dq.front() != tmp) {
+      dq.push_front(dq.back());
+      dq.pop_back();
+      right_move++;
+    }
+    dq.pop_front();
+    ans += min(left_move, right_move);
+  }
+  cout << ans << '\n';
+  return 0;
 }
