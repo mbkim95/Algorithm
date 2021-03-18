79d29027c525f0dde7cde94894f8ef3494e3a1ca
mbkim95 <mbkim95@naver.com>
2021-03-18 18:29:32 +0900

    Binary Search

--- Baekjoon/1920.cpp
+++ Baekjoon/1920.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
-int main() {
-	int n, m;
-	scanf("%d", &n);
-	vector<int> v(n);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &v[i]);
-	scanf("%d", &m);
-	sort(v.begin(), v.end());
-	for (int i = 0; i < m; i++) {
-		int tmp;
-		scanf("%d", &tmp);
-		printf("%d\n", binary_search(v.begin(), v.end(), tmp));
-	}
-	return 0;
+int n, m, num[100001];
+
+int find(int number) {
+    int s = 0, e = n-1;
+    
+    while(s <= e){
+        int mid = (s + e) / 2;
+        if(num[mid] == number) return 1;
+        else if(num[mid] < number) s = mid + 1;
+        else e = mid - 1;
+    }
+    return 0;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n;
+    for(int i=0; i <n; i++) cin >> num[i];
+    cin >> m;
+    sort(num, num + n);
+    for(int i=0;i <m; i++) {
+        int tmp;
+        cin >> tmp;
+        cout << find(tmp) << '\n';
+    }
+    return 0;
 }
