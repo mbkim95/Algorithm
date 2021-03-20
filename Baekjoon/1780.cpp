1573835a1d3cee3cb0c74a9ac043c79e0112e7ff
mbkim95 <mbkim95@naver.com>
2021-03-20 16:15:52 +0900

    Divide and Conquer

--- Baekjoon/1780.cpp
+++ Baekjoon/1780.cpp
@@ -1,42 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int p[2188][2188];
-int ans[3], n;
+int n, p[3000][3000], m, z, o;
 
-bool check(int y, int x, int size) {
-	int tmp = p[y][x];
+void check(int x, int y, int size) {
+  bool find = true;
+  int c = p[y][x];
+  for (int i = 0; i < size; i++)
+    for (int j = 0; j < size; j++) {
+      if (p[y + i][x + j] != c) {
+        find = false;
+        break;
+      }
+      if (!find) break;
+    }
+  if (find) {
+    if (c == -1) m++;
+    else if(c == 0) z++;
+    else o++;
+    return;
+  }
 
-	for (int i = y; i < y + size; i++) {
-		for (int j = x; j < x + size; j++) {
-			if (tmp != p[i][j])
-				return false;
-		}
-	}
-	return true;
-}
-
-void solve(int x, int y, int size) {
-	if (check(y, x, size)) {
-		ans[p[y][x] + 1]++;
-		return;
-	}
-	else {
-		int s = size / 3;
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				solve(x + s * i, y + s * j, s);
-			}
-		}
-	}
-	return;
+  int s = size / 3;
+  for(int i=0; i<9; i++)  check(x + (i % 3) * s, y + (i / 3) * s, s);
 }
 
 int main() {
-	cin >> n;
-	for (int i = 1; i <= n; i++)
-		for (int j = 1; j <= n; j++)
-			cin >> p[i][j];
-	solve(1, 1, n);
-	cout << ans[0] << "\n" << ans[1] << "\n" << ans[2] << endl;
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++) cin >> p[i][j];
+  check(0, 0, n);
+  cout << m << '\n' << z << '\n' << o << '\n';
+  return 0;
 }
