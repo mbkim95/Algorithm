507e3596584cb098d7d6abca6df4ddb48752b6c9
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-03-20 20:49:04 +0900

    Binary Search

--- Baekjoon/1654.cpp
+++ Baekjoon/1654.cpp
@@ -1,35 +1,26 @@
+#include <algorithm>
 #include <iostream>
-#include <vector> 
-#include <limits>
 using namespace std;
 
+long long k, n, ans, l[10001];
+
 int main() {
-	int k, n;
-	cin >> k >> n;
-	vector<int> len(k);
-	for (int i = 0; i < k; i++)
-		cin >> len[i];
+  cin >> k >> n;
+  for (int i = 0; i < k; i++) cin >> l[i];
 
-	long long left = 0;
-	long long right = numeric_limits<long long>::max();
-	long long ans = 0;
-
-	while (left <= right) {
-		long long mid = (left + right) / 2;
-		int result = 0;
-
-		for (int i = 0; i < len.size(); i++) {
-			result += len[i] / mid;
-		}
-
-		if (result >= n) {
-			left = mid + 1;
-			if (mid > ans)
-				ans = mid;
-		}
-		else
-			right = mid - 1;
-	}
-	cout << ans << '\n';
-	return 0;
+  long long s = 1, e = l[0];
+  for(int i=1; i<k; i++) e = max(e, l[i]);
+  
+  while(s <= e) {
+    long long mid = (s + e) / 2;
+    long long cnt = 0;
+    for(int i=0;i <k; i++) cnt += l[i] / mid;
+    if(cnt < n) e = mid -1;
+    else{
+      s = mid + 1;
+      ans = max(ans, mid);
+    }
+  }
+  cout << ans << '\n';
+  return 0;
 }
