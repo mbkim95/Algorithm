1b1fb293f4393401794674767eac0f8693dbacac
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-03-21 20:41:12 +0900

    Binary Search

--- Baekjoon/2805.cpp
+++ Baekjoon/2805.cpp
@@ -1,48 +1,31 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
-int n, m;
-vector<int> tree;
-
-bool findHeight(int h) {
-	long long sum = 0;
-	for (int i = 0; i < n; i++) {
-		if (h < tree[i])
-			sum += tree[i] - h;
-	}
-	return sum >= m;
-}
-
-int solve() {
-	int left = 0;
-	int right = tree[n - 1];
-	int ans = 0;
-	while (left <= right) {
-		int mid = (left + right) / 2;
-		if (findHeight(mid)) {
-			if(ans < mid)
-				ans = mid;
-			left = mid + 1;
-		}
-		else 
-			right = mid - 1;
-	}
-	return ans;
-}
+int n, m, ans, t[1000001];
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> n >> m;
-	tree = vector<int>(n);
-	int right = 0;
-	for (int i = 0; i < n; i++) {
-		cin >> tree[i];
-	}
-	sort(tree.begin(), tree.end());
-	cout << solve() << '\n';
-	return 0;
+  cin >> n >> m;
+
+  int l = 1, r = 1;
+  for (int i = 0; i < n; i++) {
+    cin >> t[i];
+    r = max(r, t[i]);
+  }
+  while (l <= r) {
+    long long sum = 0;
+    int mid = (l + r) / 2;
+
+    for (int i = 0; i < n; i++) {
+      if (mid < t[i]) sum += (t[i] - mid);
+    }
+
+    if (sum < m) r = mid - 1;
+    else {
+      l = mid + 1;
+      ans = max(ans, mid);
+    }
+  }
+  cout << ans << '\n';
+  return 0;
 }
