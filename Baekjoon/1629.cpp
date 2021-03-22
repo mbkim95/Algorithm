26fbaa91d1109c3d062ac2111522f75ef956ae8a
mbkim95 <mbkim95@naver.com>
2021-03-22 23:45:24 +0900

    Divide and Conquer

--- Baekjoon/1629.cpp
+++ Baekjoon/1629.cpp
@@ -3,22 +3,21 @@ using namespace std;
 
 long long a, b, c;
 
-long long solve(long long n, long long k) {
-	if (k == 0)
-		return 1;
+long long mul(long long n, long long m) {
+  if (m == 0) return 1;
+  if (m == 1) return n;
+  if (m % 2 == 1) return mul(n, m - 1) * n;
 
-	long long tmp = solve(n, k / 2);
-	long long result = tmp * tmp % c;
-
-	if (k % 2 == 1)
-		result = result * n % c;
-
-	return result;
+  long long h = mul(n, m / 2);
+  h %= c;
+  return (h * h) % c;
 }
 
-
 int main() {
-	cin >> a >> b >> c;
-	cout << solve(a, b) << endl;
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
 
+  cin >> a >> b >> c;
+  cout << mul(a, b) % c << '\n';
+  return 0;
 }
