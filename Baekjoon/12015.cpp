882404cf72a7be4c502400d01834e88986791523
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-03-24 21:27:28 +0900

    LIS

--- Baekjoon/12015.cpp
+++ Baekjoon/12015.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
-int n;
-int a[1000001];
-vector<int> lis;
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
 
-int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+  int n;
+  cin >> n;
+  vector<int> num(n);
+  for(int i=0; i<n; i++) cin >> num[i];
 
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> a[i];
-	lis.push_back(-1);
-	for (int i = 0; i < n; i++) {
-		if (lis.back() < a[i]) lis.push_back(a[i]);
-		else {
-			auto it = lower_bound(lis.begin(), lis.end(), a[i]);
-			*it = a[i];
-		}
-	}
-	cout << lis.size() - 1 << '\n';
-	return 0;
+  vector<int> ans;
+  ans.push_back(num[0]);
+
+  for(int i=1; i<n; i++){
+    if(num[i] > ans.back()) ans.push_back(num[i]);
+    else{
+      auto it = lower_bound(ans.begin(), ans.end(), num[i]);
+      *it = num[i];
+    }
+  }
+  cout << ans.size() << '\n';
+  return 0;
 }
