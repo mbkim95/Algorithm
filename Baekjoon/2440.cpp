7dab9427a8077bbe8f8ab1fd2f8dde969659b42c
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-03-26 21:02:10 +0900

    Implementation

--- Baekjoon/2440.cpp
+++ Baekjoon/2440.cpp
@@ -1,12 +1,11 @@
-#include <cstdio>
+#include <iostream>
+using namespace std;
 
 int main() {
-	int n;
-	scanf("%d", &n);
-	for (int i = n; i > 0; i--) {
-		for (int j = i; j > 0; j--) {
-			printf("*");
-		}
-		printf("\n");
-	}
+    int n;
+    cin >> n;
+    for(int i=n; i>0; i--) {
+        for(int j=0; j<i; j++) cout << '*';
+        cout << '\n';
+    }
 }
