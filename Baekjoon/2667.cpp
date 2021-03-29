f4fb3db9bbc898f658f08cb003f60a257c7d92ec
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-03-29 23:43:01 +0900

    DFS

--- Baekjoon/2667.cpp
+++ Baekjoon/2667.cpp
@@ -1,55 +1,42 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-int n, cnt;
-vector<vector<int>> mat;
+int n, board[26][26], cnt;
+bool visited[26][26];
 vector<int> ans;
-const int dx[4] = { -1, 1, 0, 0 }, dy[4] = { 0, 0, -1, 1 };
-
-bool inRange(int x, int y) {
-	return (0 <= x && x < n) && (0 <= y && y < n);
-}
+const int dx[] = {-1, 0, 1, 0}, dy[] = {0, -1, 0, 1};
 
 void dfs(int x, int y) {
-	mat[y][x] = 0;
-	cnt++;
+  visited[y][x] = true;
+  cnt++;
 
-	for (int i = 0; i < 4; i++) {
-		int nextX = x + dx[i];
-		int nextY = y + dy[i];
-		if (inRange(nextX, nextY) && mat[nextY][nextX]) {
-			dfs(nextX, nextY);
-		}
-	}
-}
+  for (int i = 0; i < 4; i++) {
+    int nx = x + dx[i];
+    int ny = y + dy[i];
+    if (nx < 0 || nx > n - 1 || ny < 0 || ny > n - 1) continue;
 
-void solve() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (mat[i][j]) {
-				dfs(j, i);
-				ans.push_back(cnt);
-				cnt = 0;
-			}
-		}
-	}
-	sort(ans.begin(), ans.end());
-	printf("%d\n", ans.size());
-	for (int i = 0; i < ans.size(); i++) {
-		printf("%d\n", ans[i]);
-	}
+    if (!visited[ny][nx] && board[ny][nx]) {
+      dfs(nx, ny);
+    }
+  }
 }
 
 int main() {
-	scanf("%d", &n);
-	mat = vector<vector<int>>(n, vector<int>(n, 0));
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			scanf("%1d", &mat[i][j]);
-		}
-	}
-	solve();
-	return 0;
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++) scanf("%1d", &board[i][j]);
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      if (!visited[i][j] && board[i][j]) {
+        dfs(j, i);
+        ans.push_back(cnt);
+        cnt = 0;
+      }
+  sort(ans.begin(), ans.end());
+  printf("%d\n", ans.size());
+  for (int i : ans) printf("%d\n", i);
+  return 0;
 }
