dad1ac2bc5984dfa1dd7d1d071218ea7ca71fac9
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-04-08 22:27:03 +0900

    BFS

--- Baekjoon/7569.cpp
+++ Baekjoon/7569.cpp
@@ -1,81 +1,59 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <queue>
 using namespace std;
 
-int n, m, h;
-vector<vector<vector<int>>> map;
-queue<pair<int, pair<int, int>>>q;
-const int dx[] = { -1, 0, 1, 0, 0, 0 }, dy[] = { 0, -1, 0, 1, 0, 0 }, dz[] = { 0, 0, 0, 0, -1, 1 };
+struct tomato {
+  int x, y, z;
+};
 
-bool inRange(int x, int y, int z) {
-	return (0 <= x && x < m) && (0 <= y && y < n) && (0 <= z && z < h);
-}
-
-bool isFinished() {
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < n; j++) {
-			for (int k = 0; k < m; k++) {
-				if (map[i][j][k] == 0)
-					return false;
-			}
-		}
-	}
-	return true;
-}
+int n, m, h, mt, map[101][101][101], v[101][101][101];
+const int dx[] = {-1, 0, 1, 0, 0, 0}, dy[] = {0, -1, 0, 1, 0, 0}, dz[] = {0, 0, 0, 0, -1, 1};
+queue<tomato> q;
 
 int bfs() {
-	int time = 0;
+  int day = 0, cnt = 0;
+  if(cnt == mt)  return 0;
+  while (!q.empty()) {
+    int size = q.size();   
+    if(cnt == mt)  return day;
+    while (size--) {
+      tomato cur = q.front();
+      q.pop();
 
-	while (true) {
-		if (isFinished()) {
-			return time;
-		}
-		else {
-			if (q.empty()) {
-				return -1;
-			}
-		}
-		int size = q.size();
-		while (size--) {
+      for (int i = 0; i < 6; i++) {
+        int nx = cur.x + dx[i];
+        int ny = cur.y + dy[i];
+        int nz = cur.z + dz[i];
 
-			pair<int, pair<int, int>> tmp = q.front();
-			q.pop();
-
-			for (int i = 0; i < 6; i++) {
-				int x = tmp.first + dx[i];
-				int y = tmp.second.first + dy[i];
-				int z = tmp.second.second + dz[i];
-
-				if (inRange(x, y, z) && map[z][y][x] == 0) {
-					q.push(make_pair(x, make_pair(y, z)));
-					map[z][y][x] = 1;
-				}
-			}
-		}
-		time++;
-	}
-	return time;
+        if (nx < 0 || nx > m - 1 || ny < 0 || ny > n - 1 || nz < 0 || nz > h - 1) continue;
+        if (!v[nz][ny][nx] && !map[nz][ny][nx]) {
+          tomato t = {nx, ny, nz};
+          q.push(t);
+          cnt++;
+          v[nz][ny][nx] = 1;
+        }
+      }
+    }
+    day++; 
+  }
+  return -1;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	cin >> m >> n >> h;
-	map = vector<vector<vector<int>>>(h, vector<vector<int>>(n, vector<int >(m, 0)));
-
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < n; j++) {
-			for (int k = 0; k < m; k++) {
-				cin >> map[i][j][k];
-				if (map[i][j][k] == 1) {
-					q.push(make_pair(k, make_pair(j, i)));
-				}
-			}
-		}
-	}
-	cout << bfs() << '\n';
-	return 0;
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> m >> n >> h;
+  for (int i = 0; i < h; i++)
+    for (int j = 0; j < n; j++)
+      for (int k = 0; k < m; k++) {
+        cin >> map[i][j][k];
+        if (map[i][j][k] == 1) {
+          v[i][j][k] = 1;
+          tomato t = {k, j, i};
+          q.push(t);        
+        }
+        if(map[i][j][k] == 0) mt++;
+      }
+  cout << bfs() << '\n';
+  return 0;
 }
