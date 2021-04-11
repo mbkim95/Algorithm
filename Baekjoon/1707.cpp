4fc18468e9b79c65754bbcee178066ec1d14cc2d
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-04-11 21:20:09 +0900

    BFS

--- Baekjoon/1707.cpp
+++ Baekjoon/1707.cpp
@@ -1,79 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
 using namespace std;
 
-int v, e;
-vector<int> adj[20001];
-int visited[20001];
-bool visited2[20001];
-bool is_possible = true;
+int v, e, c[20001];
+vector<vector<int> > adj;
 
-void dfs(int idx, int chk) {
-	visited[idx] = chk;
+bool bfs(int start){
+  queue<int> q;
+  q.push(start);
+  c[start] = 1;
 
-	for (int next : adj[idx]) {
-		if (visited[next] == 0) {
-			if (chk == 1) dfs(next, 2);
-			else dfs(next, 1);
-		}
-	}
-}
+  while(!q.empty()){
+    int cur = q.front();
+    q.pop();
 
-void bfs(int start) {
-	queue<pair<int, int>> q;	
-	q.push({ start, visited[start] });
-	
-	visited2[start] = true;
-
-	while (!q.empty()) {
-		int cur = q.front().first;
-		int chk = q.front().second;
-		q.pop();
-
-		for (int next : adj[cur]) {
-			int next_chk = visited[next];
-			if (chk == next_chk) {
-				is_possible = false;
-				return;
-			}
-			if (!visited2[next]) {
-				visited2[next] = true;
-				q.push({ next, next_chk });
-			}
-		}
-	}
-	return;
+    for(int i : adj[cur]) {
+      if(!c[i]){
+        q.push(i);
+        if(c[cur] == 1) c[i] = 2;
+        else c[i] = 1;
+      }
+      else if(c[cur] == c[i]) return false;
+    }
+  }
+  return true;
 }
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
 
-	int k;
-	cin >> k;
-	while (k--) {
-		cin >> v >> e;
-		is_possible = true;
-		for (int i = 0; i < 20001; i++) adj[i].clear();
-		fill(visited, visited + 20001, 0);
-		fill(visited2, visited2 + 20001, false);
-		for (int i = 0; i < e; i++) {
-			int a, b;
-			cin >> a >> b;
-			adj[a].push_back(b);
-			adj[b].push_back(a);
-		}
-		for (int i = 1; i <= v; i++) {
-			if (!visited[i])
-				dfs(i, 1);
-		}
-		for (int i = 1; i <= v; i++) {
-			if (is_possible && !visited2[i])
-				bfs(i);
-		}
-		if (is_possible) cout << "YES\n";
-		else cout << "NO\n";
-	}
-	return 0;
+  int k;
+  cin >> k;
+  while (k--) {
+    cin >> v >> e;
+    adj = vector<vector<int> >(v+1);
+    memset(c, 0, sizeof(c));
+    for (int i = 0; i < e; i++) {
+      int a, b;
+      cin >> a >> b;
+      adj[a].push_back(b);
+      adj[b].push_back(a);
+    }
+    bool ans = true;
+    for(int i=1; i<=v; i++){
+      if(!c[i]) ans &= bfs(i);
+    }
+    if(ans) cout << "YES\n";
+    else cout << "NO\n";
+  }
+  return 0;
 }
