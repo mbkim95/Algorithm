bfbc026d0e26ed88f460c94dd4ae63cded6ef858
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-04-15 20:53:47 +0900

    Implementation

--- Baekjoon/2442.cpp
+++ Baekjoon/2442.cpp
@@ -2,15 +2,12 @@
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
-
-	for (int i = 1; i <= n; i++) {
-		for (int k = n - i; k > 0; k--)
-			cout << ' ';
-		for (int j = 0; j < 2 * i - 1; j++)
-			cout << '*';			
-		cout << '\n';
-	}
-	return 0;
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j < n - i; j++) cout << ' ';
+    for (int j = 0; j < 2 * i - 1; j++) cout << '*';
+    cout << '\n';
+  }
+  return 0;
 }
