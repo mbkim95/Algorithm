9d1a785366cda3b60a5be27ea88d0eeee4f79f82
mbkim95 <mbkim95@naver.com>
2021-04-17 00:05:28 +0900

    Priority Queue

--- Baekjoon/1927.cpp
+++ Baekjoon/1927.cpp
@@ -2,28 +2,23 @@
 #include <queue>
 using namespace std;
 
+int n, x;
+priority_queue<int> pq;
+
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int n;
-	cin >> n;
-	priority_queue<int> q;
-	for (int i = 0; i < n; i++) {
-		int tmp;
-		cin >> tmp;
-		if (tmp == 0) {
-			if (q.empty())
-				cout << "0\n";
-			else {
-				tmp = -q.top();
-				q.pop();
-				cout << tmp << '\n';
-			}
-		}
-		else {
-			q.push(-tmp);
-		}
-	}
-	return 0;
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+  while (n--) {
+    cin >> x;
+    if (x == 0) {
+      if (pq.empty()) cout << "0\n";
+      else {
+        cout << -pq.top() << '\n';
+        pq.pop();
+      }
+    } else pq.push(-x);
+  }
+  return 0;
 }
