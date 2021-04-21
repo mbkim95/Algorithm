650b7455aaba1a1c8b9b3ddc4646f038e770996b
mbkim95 <mbkim95@naver.com>
2021-04-21 22:12:44 +0900

    Priority Queue

--- Baekjoon/1655.cpp
+++ Baekjoon/1655.cpp
@@ -2,58 +2,29 @@
 #include <queue>
 using namespace std;
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+int n;
+priority_queue<int> max_heap, min_heap;
 
-	int n;
-	cin >> n;
+int main(){
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
 
-	priority_queue<int, vector<int>, greater<int>> min_heap;
-	priority_queue<int, vector<int>, less<int>> max_heap;
-	for (int i = 0; i < n; i++) {
-		int tmp;
-		cin >> tmp;
+  cin >> n;
+  for(int i=0; i<n; i++){
+    int tmp;
+    cin >> tmp;
+    if(max_heap.size() <= min_heap.size()) max_heap.push(tmp);
+    else min_heap.push(-tmp);
 
-		if (min_heap.size() == max_heap.size()) {
-			if (max_heap.size() == 0) {
-				max_heap.push(tmp);
-			}
-			else {
-				if (min_heap.top() >= tmp)
-					max_heap.push(tmp);
-				else {
-					max_heap.push(min_heap.top());
-					min_heap.pop();
-					min_heap.push(tmp);
-				}
-			}
-		}
-		else {
-			if (max_heap.size() > min_heap.size()) {
-				if (max_heap.top() > tmp) {
-					min_heap.push(max_heap.top());
-					max_heap.pop();
-					max_heap.push(tmp);
-				}
-				else {
-					min_heap.push(tmp);
-				}
-			}
-			else if (max_heap.size() < min_heap.size()) {
-				if (min_heap.top() < tmp) {
-					max_heap.push(min_heap.top());
-					min_heap.pop();
-					min_heap.push(tmp);
-				}
-				else {
-					max_heap.push(tmp);
-				}
-			}
-		}
-
-		cout << max_heap.top() << '\n';
-	}
-	return 0;
+    if(!max_heap.empty() && !min_heap.empty()){
+      if(max_heap.top() > -min_heap.top()){
+        max_heap.push(-min_heap.top());
+        min_heap.pop();
+        min_heap.push(-max_heap.top());
+        max_heap.pop();
+      }
+    }
+    cout << max_heap.top() << '\n';
+  }
+  return 0;
 }
