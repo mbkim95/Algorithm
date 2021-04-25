a8df0ec65a19e0db01fac11a0c4718e3b156c9b4
김민범 <44355413+mbkim95@users.noreply.github.com>
2021-04-25 22:10:29 +0900

    Implementation

--- Baekjoon/2748.cpp
+++ Baekjoon/2748.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 
-long long dp[91];
+int n;
+long long fib[91];
 
 int main() {
-	int n;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
 	cin >> n;
-	dp[0] = 0;
-	dp[1] = 1;
-	for (int i = 2; i <= n; i++) {
-		dp[i] = dp[i - 1] + dp[i - 2];
-	}
-	cout << dp[n] << '\n';
+	fib[1] = 1;
+	for (int i = 2; i <= n; i++) fib[i] = fib[i - 1] + fib[i - 2];
+	cout << fib[n] << '\n';
+	return 0;
 }
