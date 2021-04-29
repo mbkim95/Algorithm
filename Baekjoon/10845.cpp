18836aa6e9101646063781978940914c47e0d92c
mbkim95 <mbkim95@naver.com>
2021-04-29 11:22:34 +0900

    Queue

--- Baekjoon/10845.cpp
+++ Baekjoon/10845.cpp
@@ -1,46 +1,41 @@
 #include <iostream>
-#include <string>
 #include <queue>
 using namespace std;
 
+int n;
+
 int main() {
-	int n;
-	ios_base::sync_with_stdio(false);
-	cin >> n;
-	queue<int> q;
-	while (n--) {
-		string s;
-		cin >> s;
-		if (s == "push") {
-			int tmp;
-			cin >> tmp;
-			q.push(tmp);
-		}
-		else if (s == "pop") {
-			if (q.empty())
-				cout << -1 << '\n';
-			else {
-				cout << q.front() << '\n';
-				q.pop();
-			}
-		}
-		else if (s == "size") {
-			cout << q.size() << '\n';
-		}
-		else if (s == "empty") {
-			cout << q.empty() << '\n';
-		}
-		else if (s == "front") {
-			if (q.empty())
-				cout << -1 << '\n';
-			else
-				cout << q.front() << '\n';
-		}
-		else if (s == "back") {
-			if (q.empty())
-				cout << -1 << '\n';
-			else
-				cout << q.back() << '\n';
-		}
-	}
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+  queue<int> q;
+  for (int i = 0; i < n; i++) {
+    string cmd;
+    int tmp;
+    cin >> cmd;
+    if (cmd == "push") {
+      cin >> tmp;
+      q.push(tmp);
+    }
+    if (cmd == "pop") {
+      if (q.empty())
+        cout << "-1\n";
+      else {
+        cout << q.front() << '\n';
+        q.pop();
+      }
+    }
+    if (cmd == "front") {
+      if(q.empty()) cout << "-1\n";
+      else cout << q.front() << '\n';
+    }
+    if (cmd == "back") {
+      if (q.empty()) cout << "-1\n";
+      else cout << q.back() << '\n';
+    }
+    if (cmd == "size") cout << q.size() << '\n';
+    if (cmd == "empty") cout << q.empty() << '\n';
+  }
+  return 0;
 }
