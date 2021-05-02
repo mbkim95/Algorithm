a5f44ff4e841ec67f01d6a738318240f86cf3910
mbkim95 <mbkim95@naver.com>
2021-05-02 11:26:34 +0900

    Implementation

--- Baekjoon/2445.cpp
+++ Baekjoon/2445.cpp
@@ -2,28 +2,21 @@
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
-	for (int i = 1; i <= n - 1; i++) {
-		for (int j = 1; j <= i; j++)
-			cout << '*';
-		for (int j = 1; j <= 2 * n - 2 * i; j++)
-			cout << ' ';
-		for (int j = 1; j <= i; j++)
-			cout << '*';
-		cout << '\n';
-	}
-	for (int i = 1; i <= 2 * n; i++)
-		cout << '*';
-	cout << '\n';
-	for (int i = n - 1; i >= 1; i--) {
-		for (int j = 1; j <= i; j++)
-			cout << '*';
-		for (int j = 1; j <= 2 * n - 2 * i; j++)
-			cout << ' ';
-		for (int j = 1; j <= i; j++)
-			cout << '*';
-		cout << '\n';
-	}
-	return 0;
+  int n;
+  cin >> n;
+  for (int i = 1; i <= 2 * n - 1; i++) {
+    if (i < n) {
+      for (int j = 0; j < i; j++) cout << '*';
+      for (int j = 0; j < 2 * n - 2 * i; j++) cout << ' ';
+      for (int j = 0; j < i; j++) cout << '*';
+    } else if (i == n)
+      for (int j = 0; j < 2 * n; j++) cout << '*';
+    else {
+      for (int j = 0; j < 2 * n - i; j++) cout << '*';
+      for (int j = 0; j < 2 * (i - n); j++) cout << ' ';
+      for (int j = 0; j < 2 * n - i; j++) cout << '*';
+    }
+    cout << '\n';
+  }
+  return 0;
 }
