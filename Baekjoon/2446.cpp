eeb12963e8f06a2b52ebad69287a3f537bba045d
mbkim95 <mbkim95@naver.com>
2021-05-03 21:11:57 +0900

    Implementation

--- Baekjoon/2446.cpp
+++ Baekjoon/2446.cpp
@@ -2,28 +2,17 @@
 using namespace std;
 
 int main() {
-	int n;
-	cin >> n;
-	for (int i = 1; i <= n - 1; i++) {
-		for (int j = 1; j <= i - 1; j++)
-			cout << ' ';
-		for (int j = 1; j <= 2 * (n + 1 - i) - 1; j++)
-			cout << '*';
-		cout << '\n';
-	}
-	for (int i = 1; i <= 2 * n - 1; i++) {
-		if (i < n)
-			cout << ' ';
-		else if (i == n)
-			cout << '*';
-	}
-	cout << '\n';
-	for (int i = n - 1; i >= 1; i--) {
-		for (int j = 1; j <= i - 1; j++)
-			cout << ' ';
-		for (int j = 1; j <= 2 * (n + 1 - i) - 1; j++)
-			cout << '*';
-		cout << '\n';
-	}
-	return 0;
+  int n;
+  cin >> n;
+  for (int i = 1; i <= 2 * n - 1; i++) {
+    if (i <= n) {
+      for (int j = 1; j < i; j++) cout << ' ';
+      for (int j = 0; j <= 2 * n - 2 * i; j++) cout << '*';
+    } else {
+      for (int j = 0; j < 2 * n - 1 - i; j++) cout << ' ';
+      for (int j = 0; j <= 2 * (i - n); j++) cout << '*';
+    }
+    cout << '\n';
+  }
+  return 0;
 }
