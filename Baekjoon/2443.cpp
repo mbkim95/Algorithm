014f5e938678090665a09fda43c169d1936c37b5
mbkim95 <mbkim95@naver.com>
2021-05-08 21:10:11 +0900

    Implementation

---
+++ Baekjoon/2443.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i - 1; j++) cout << ' ';
+    for (int j = 1; j <= 2 * (n - i) + 1; j++) cout << '*';
+    cout << '\n';
+  }
+  return 0;
+}
