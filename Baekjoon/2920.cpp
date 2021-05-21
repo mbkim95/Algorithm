61151f29636bad5e1ac0be422844d223158e85ec
mbkim95 <mbkim95@naver.com>
2021-05-21 21:37:20 +0900

    Implementation

--- Baekjoon/2920.cpp
+++ Baekjoon/2920.cpp
@@ -1,42 +1,20 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
 
+int m[8];
+
 int main() {
-	vector<int> melody(8);
-	vector<int> temp;
-	for (int i = 0; i < 8; i++)
-		cin >> melody[i];
-
-	temp = melody;
-	sort(temp.begin(), temp.end());
-
-	bool find = true;
-	for (int i = 0; i < 8; i++) {
-		if (melody[i] != temp[i]) {
-			find = false;
-			break;
-		}
-	}
-
-	if (!find) {
-		find = true;
-		reverse(temp.begin(), temp.end());
-		for (int i = 0; i < 8; i++) {
-			if (melody[i] != temp[i]) {
-				find = false;
-				break;
-			}
-		}
-
-		if (find)
-			cout << "descending" << '\n';
-		else
-			cout << "mixed" << '\n';
-	}
-	else {
-		cout << "ascending" << '\n';
-	}
-	return 0;
+  bool is_ascending = true, is_descending = true;
+  int prev, cur;
+  cin >> prev;
+  for(int i=0; i<7; i++) {
+    cin >> cur;
+    if(prev < cur) is_descending = false;
+    if(cur < prev) is_ascending = false;
+    prev = cur;
+  }
+  if(!is_ascending && !is_descending) cout << "mixed\n";
+  else if(is_ascending) cout << "ascending\n";
+  else cout << "descending\n";
+  return 0;
 }
