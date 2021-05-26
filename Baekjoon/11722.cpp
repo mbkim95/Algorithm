3905a2d94eaea7c4a1d7d6c6bf5ad3c84726c73a
mbkim95 <mbkim95@naver.com>
2021-05-26 22:00:38 +0900

    LIS

--- Baekjoon/11722.cpp
+++ Baekjoon/11722.cpp
@@ -1,45 +1,24 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-int n;
-vector<int> arr, dp;
-
-int solve(int idx) {
-	int& ret = dp[idx];
-
-	if (ret != -1)
-		return ret;
-
-	ret = 1;
-
-	for (int i = idx + 1; i < n; i++) {
-		if (arr[idx] > arr[i]) {
-			ret = max(ret, 1 + solve(i));
-		}
-	}
-	return ret;
-}
+int n, num[1001];
+vector<int> ans;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> num[i];
+  reverse(num, num + n);
+  ans.push_back(num[0]);
 
-	cin >> n;
-	arr = vector<int>(n);
-	dp = vector<int>(n, -1);
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-
-	for (int i = 0; i < n; i++)
-		solve(i);
-
-	int ans = 0;
-	for (int i = 0; i < n; i++) {
-		ans = max(ans, dp[i]);
-	}
-	cout << ans << '\n';
-	return 0;
+  for(int i=1; i<n; i++) {
+    if(ans.back() < num[i]) ans.push_back(num[i]);
+    else{
+      auto it = lower_bound(ans.begin(), ans.end(), num[i]);
+      *it = num[i];
+    }
+  }
+  cout << ans.size() << '\n';
+  return 0;
 }
