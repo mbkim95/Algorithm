2b53b20ab845c63ac6c932fa977a1aeb16b9dd67
mbkim95 <mbkim95@naver.com>
2021-05-27 23:36:16 +0900

    Implementation

--- Baekjoon/1475.cpp
+++ Baekjoon/1475.cpp
@@ -1,43 +1,24 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
 
-int n;
-vector<int> num;
+int num[10];
 
-void plusSet(){
-    for(int i=0; i<10; i++)
-        num[i]++;
-}
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> n;
-    num = vector<int>(10, 1);    
-    int ans = 1;
-    while(n > 0){
-        int tmp = n % 10;
-        n /= 10;
-        bool flag = 0;
-        if(num[tmp] == 0){
-            if(tmp == 6 && num[9] != 0){
-                num[9]--;
-                flag = 1;
-            }else if(tmp == 9 && num[6] != 0){
-                num[6]--;            
-                flag = 1;
-            }
-            if(!flag){
-                plusSet();
-                ans++;
-            }
-        }
-
-        if(!flag)
-            num[tmp]--;
-    }
-    cout << ans << '\n';
-    return 0;
+int main() {
+  string n;
+  cin >> n;
+  for (int i = 0; i < n.length(); i++) {
+    int tmp = n[i] - '0';
+    if (tmp == 6 || tmp == 9) {
+      if (num[6] <= num[9])
+        num[6]++;
+      else
+        num[9]++;
+    } else
+      num[tmp]++;
+  }
+  int ans = 0;
+  for (int i = 0; i < 10; i++) ans = max(ans, num[i]);
+  cout << ans << '\n';
+  return 0;
 }
