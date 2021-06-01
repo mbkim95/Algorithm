805804b23ab3b1a3fcf0691d1adc1f346b43725f
mbkim95 <mbkim95@naver.com>
2021-06-01 23:15:23 +0900

    Implementation

--- Baekjoon/4101.cpp
+++ Baekjoon/4101.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int n1, n2;
-	while (true) {
-		cin >> n1 >> n2;
-		if (n1 == 0 && n2 == 0) break;
-		if (n1 > n2)cout << "Yes\n";
-		else cout << "No\n";
-	}
-	return 0;
+int main(){
+  while(true){
+    int a, b;
+    cin >> a >> b;
+    if(a == 0 && b == 0) break;
+    else if(a > b) cout << "Yes\n";
+    else cout << "No\n";
+  }
+  return 0;
 }
