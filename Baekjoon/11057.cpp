28091f9b646a73f89e171bb16edaef7a6e61b19e
mbkim95 <mbkim95@naver.com>
2021-06-08 20:32:16 +0900

    Dynamic Programming

--- Baekjoon/11057.cpp
+++ Baekjoon/11057.cpp
@@ -1,26 +1,18 @@
 #include <iostream>
-#define MOD 10007
 using namespace std;
 
-int n, dp[1001][10];
+int n, num[1001][10];
 
-int main() {
-	cin >> n;	
+int main(){ 
+  cin >> n;
+  for(int i=0; i<10; i++) num[1][i] = 1;
+  for(int i=2; i<=n; i++)
+    for(int j=0; j<10; j++)
+      for(int k=0; k<=j; k++)
+        num[i][j] = (num[i][j] + num[i-1][k])  % 10007;
 
-	for (int i = 0; i <10; i++)
-		dp[1][i] = 1;
-
-	for (int i = 2; i <= n; i++) {
-		for (int j = 0; j < 10; j++) {
-			for (int k = j; k < 10; k++) {
-				dp[i][j] = (dp[i][j] + dp[i - 1][k]) % MOD;
-			}			
-		}
-	}
-	int sum = 0;
-
-	for (int i = 0; i < 10; i++)
-		sum = (sum + dp[n][i]) % MOD;
-	cout << sum % MOD << endl;
-
+  int sum = 0;
+  for(int i=0; i<10; i++) sum = (sum + num[n][i]) % 10007;
+  cout << sum << '\n';
+  return 0;
 }
