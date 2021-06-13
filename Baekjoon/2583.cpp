b279cf6a26d2571f9e225fce57e175169908fee8
mbkim95 <mbkim95@naver.com>
2021-06-13 12:15:54 +0900

    DFS

--- Baekjoon/2583.cpp
+++ Baekjoon/2583.cpp
@@ -1,60 +1,44 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-int n, m, k, cnt;
+int n, m, k, t;
+int b[101][101], chk[101][101];
+const int dx[] = {-1, 0, 1, 0}, dy[] = {0, -1, 0, 1};
 vector<int> ans;
-vector<vector<int>> map;
-const int dx[4] = { -1, 1, 0, 0 }, dy[4] = { 0, 0, -1, 1 };
-
-void setSquare(int x1, int y1, int x2, int y2) {
-	for (int i = y1; i <= y2; i++) {
-		for (int j = x1; j <= x2; j++) {
-			map[i][j] = 1;
-		}
-	}
-}
-
-bool inRange(int x, int y) {
-	return (0 <= x && x < n) && (0 <= y && y < m);
-}
 
 void dfs(int x, int y) {
-	map[y][x] = 2;
-	cnt++;
-	for (int i = 0; i < 4; i++) {
-		int nextX = x + dx[i];
-		int nextY = y + dy[i];
-		if (inRange(nextX, nextY) && !map[nextY][nextX]) {
-			dfs(nextX, nextY);
-		}
-	}
+  t++;
+  chk[y][x] = 1;
+  for (int i = 0; i < 4; i++) {
+    int nx = x + dx[i];
+    int ny = y + dy[i];
+    if (nx < 0 || nx > n - 1 || ny < 0 || ny > m - 1) continue;
+    if (!b[ny][nx] && !chk[ny][nx]) dfs(nx, ny);
+  }
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> m >> n >> k;
-	map = vector<vector<int>>(m, vector<int>(n, 0));
-	for (int i = 0; i < k; i++) {
-		int x1, x2, y1, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
-		setSquare(x1, y1, x2-1, y2-1);
-	}
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			if (!map[i][j]) {
-				dfs(j, i);
-				ans.push_back(cnt);
-				cnt = 0;
-			}
-		}
-	}
-	sort(ans.begin(), ans.end());
-	cout << ans.size() << '\n';
-	for (auto x : ans)
-		cout << x << ' ';
-	cout << '\n';
-	return 0;
+  cin >> m >> n >> k;
+  for (int i = 0; i < k; i++) {
+    int lx, ly, rx, ry;
+    cin >> lx >> ly >> rx >> ry;
+    for (int i = ly; i < ry; i++)
+      for (int j = lx; j < rx; j++) b[i][j] = 1;
+  }
+
+  for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++) {
+      if (!b[i][j] && !chk[i][j]) {
+        dfs(j, i);
+        ans.push_back(t);
+        t = 0;
+      }
+    }
+  sort(ans.begin(), ans.end());
+  cout << ans.size() << '\n';
+  for (int i : ans) cout << i << ' ';
+  cout << '\n';
+  return 0;
 }
