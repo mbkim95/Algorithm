9421a318955581de673ba48ffb33a7087294f3c6
mbkim95 <mbkim95@naver.com>
2021-06-20 23:26:27 +0900

    Implementation

--- Baekjoon/1100.cpp
+++ Baekjoon/1100.cpp
@@ -1,28 +1,18 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-vector<vector<char>> b;
-
 int main() {
-	b = vector<vector<char>>(8, vector<char>(8));
-
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			cin >> b[i][j];
-		}
-	}
-	int cnt = 0;
-	for (int i = 0; i < 8; i++) {
-		int j;
-		if (i % 2)
-			j = 1;
-		else
-			j = 0;
-		for (; j < 8; j += 2) {
-			if (b[i][j] == 'F')
-				cnt++;
-		}
-	}
-	cout << cnt << '\n';
+  int ans = 0;
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      char c;
+      cin >> c;
+      if (c == 'F') {
+        if (!(i % 2) && !(j % 2)) ans++;
+        if (i % 2 && j % 2) ans++;
+      }
+    }
+  }
+  cout << ans << '\n';
+  return 0;
 }
