a13c7b932554e933397d1c6dba2fe1fd048159d4
mbkim95 <mbkim95@naver.com>
2021-06-22 21:43:14 +0900

    Dynamic Programming

--- Baekjoon/2193.cpp
+++ Baekjoon/2193.cpp
@@ -2,15 +2,12 @@
 using namespace std;
 
 int n;
-long dp[91];
+long long dp[91];
 
 int main() {
-	cin >> n;
-	dp[1] = 1;
-	dp[2] = 1;
-
-	for (int i = 3; i <= n; i++)
-		dp[i] = dp[i - 1] + dp[i - 2];
-		
-	cout << dp[n] << endl;
+  cin >> n;
+  dp[1] = dp[2] = 1;
+  for (int i = 3; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2];
+  cout << dp[n] << '\n';
+  return 0;
 }
