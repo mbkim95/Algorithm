0d4336a8a05c2b0d5326d0426fdbfcb3e2782fbd
mbkim95 <mbkim95@naver.com>
2021-06-23 23:08:09 +0900

    DFS

--- Baekjoon/11724.cpp
+++ Baekjoon/11724.cpp
@@ -1,39 +1,34 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int n, m;
-vector<vector<int>> adj;
-vector<bool> visited;
+int n, m, ans;
+bool adj[1001][1001], v[1001];
 
-void dfs(int idx) {
-	visited[idx] = 1;
+void dfs(int cur) {
+  v[cur] = true;
 
-	for (int i = 0; i < adj[idx].size(); i++) {
-		if (!visited[adj[idx][i]])
-			dfs(adj[idx][i]);
-	}
+  for(int i=1; i<=n; i++) {
+    if(i == cur) continue;
+    if(!v[i] && adj[cur][i]) {
+      dfs(i);
+    }
+  }
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> n >> m;
-	adj = vector<vector<int>>(n + 1, vector<int>());
-	visited = vector<bool>(n + 1, false);
-	for (int i = 0; i < m; i++) {
-		int tmp1, tmp2;
-		cin >> tmp1 >> tmp2;
-		adj[tmp1].push_back(tmp2);
-		adj[tmp2].push_back(tmp1);
-	}
-	int ans = 0;
-	for (int i = 1; i <= n; i++) {
-		if (!visited[i]) {
-			dfs(i);
-			ans++;
-		}
-	}
-	cout << ans << '\n';
-	return 0;
+  cin >> n >> m;
+  for(int i=0; i<m; i++){
+    int u, v;
+    cin >> u >> v;
+    adj[u][v] = adj[v][u] = true;
+  }
+
+  for(int i=1; i<=n; i++) {
+    if(!v[i]) {
+      ans++;
+      dfs(i);
+    }
+  }
+  cout << ans << '\n';
+  return 0;
 }
