e3a9c26ead97a56e50287d5b25bf4bae4bc44f1b
mbkim95 <mbkim95@naver.com>
2021-06-24 21:11:07 +0900

    Implementation

--- Baekjoon/1026.cpp
+++ Baekjoon/1026.cpp
@@ -1,28 +1,17 @@
-#include <iostream>
 #include <algorithm>
-#include <vector>
+#include <iostream>
 using namespace std;
 
-int n;
-vector<int> a, b;
+int n, a[51], b[51];
 
 int main() {
-	cin >> n;
-	a = vector<int>(n);
-	b = vector<int>(n);
-	for (int i = 0; i < n; i++)
-		cin >> a[i];
-
-	for (int i = 0; i < n; i++)
-		cin >> b[i];
-
-	sort(a.begin(), a.end());
-	sort(b.begin(), b.end(), greater<int>());
-
-	int ans = 0;
-	for (int i = 0; i < n; i++) {
-		ans += a[i] * b[i];
-	}
-	cout << ans << '\n';
-	return 0;
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> a[i];
+  for (int i = 0; i < n; i++) cin >> b[i];
+  sort(a, a + n);
+  sort(b, b + n, greater<int>());
+  int sum = 0;
+  for (int i = 0; i < n; i++) sum += a[i] * b[i];
+  cout << sum << '\n';
+  return 0;
 }
