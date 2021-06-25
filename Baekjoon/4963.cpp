9bf6ef16df161098b516a3b8c7eae22bb72dfe4c
mbkim95 <mbkim95@naver.com>
2021-06-25 22:25:22 +0900

    DFS

--- Baekjoon/4963.cpp
+++ Baekjoon/4963.cpp
@@ -2,56 +2,43 @@
 #include <vector>
 using namespace std;
 
-int w, h;
-vector<vector<int>> map, visited;
-const int dx[] = { -1, 0, 1, 0, -1, -1, 1, 1 }, dy[] = { 0, -1, 0, 1, -1, 1, -1, 1 };
-
-bool inRange(int x, int y) {
-	return (0 <= x && x < w) && (0 <= y && y < h);
-}
+int w, h, ans;
+vector<vector<int> > land, chk;
+const int dx[] = {-1, 0, 1, 0, 1, 1, -1, -1}, dy[] = {0, -1, 0, 1, 1, -1, 1, -1};
 
 void dfs(int x, int y) {
-	visited[y][x] = 1;
+  chk[y][x] = 1;
 
-	for (int i = 0; i < 8; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if (inRange(nx, ny) && !visited[ny][nx] && map[ny][nx] == 1) {
-			dfs(nx, ny);
-		}
-	}
-}
-
-int solve() {
-	int ans = 0;
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w; j++) {
-			if (!visited[i][j] && map[i][j]) {
-				dfs(j, i);
-				ans++;
-			}
-		}
-	}
-	return ans;
+  for (int i = 0; i < 8; i++) {
+    int nx = x + dx[i];
+    int ny = y + dy[i];
+    if (nx < 0 || nx > w - 1 || ny < 0 || ny > h - 1) continue;
+    if (!chk[ny][nx] && land[ny][nx]) {
+      dfs(nx, ny);
+    }
+  }
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+  while (true) {
+    cin >> w >> h;
+    if (w == 0 && h == 0) break;
+    ans = 0;
+    land = vector<vector<int> >(h, vector<int>(w, 0));
+    chk = vector<vector<int> >(h, vector<int>(w, 0));
+    for (int i = 0; i < h; i++) {
+      for (int j = 0; j < w; j++) cin >> land[i][j];
+    }
 
-	w = 1, h = 1;
-
-	while (true) {
-		cin >> w >> h;
-		if (w == 0 && h == 0)
-			break;
-		map = vector<vector<int>>(h, vector<int>(w, 0));
-		visited = vector<vector<int>>(h, vector<int>(w, 0));
-		for (int i = 0; i < h; i++) 
-			for (int j = 0; j < w; j++)
-				cin >> map[i][j];				
-		cout << solve() << '\n';
-	}
-	return 0;
+    for (int i = 0; i < h; i++) {
+      for (int j = 0; j < w; j++) {
+        if (!chk[i][j] && land[i][j]) {
+          ans++;
+          dfs(j, i);
+        }
+      }
+    }
+    cout << ans << '\n';
+  }
+  return 0;
 }
