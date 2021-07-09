6168ec40878a94bcbda147545caeb744fe3491b3
mbkim95 <mbkim95@naver.com>
2021-07-09 23:17:19 +0900

    BFS

--- Baekjoon/2644.cpp
+++ Baekjoon/2644.cpp
@@ -1,55 +1,48 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 using namespace std;
 
-int n, m, p1, p2;
-vector<int> visited;
-vector<vector<int>> adj;
+int n, m, x, y, v[101];
+vector<vector<int> > adj;
 
 int bfs() {
-	queue<int> q;
-	q.push(p1);
-	visited[p1] = 1;
-	int depth = 0;
+  queue<int> q;
+  q.push(x);
+  v[x] = 1;
+  int ans = 0;
 
-	while (!q.empty()) {
-		int size = q.size();
+  while (!q.empty()) {
+    int size = q.size();
+    while (size--) {
+      int cur = q.front();
+      q.pop();
 
-		for (int i = 0; i < size; i++) {
-			int tmp = q.front();
-			q.pop();
+      if (cur == y) return ans;
 
-			if (tmp == p2)
-				return depth;
-
-			for (int j = 0; j < adj[tmp].size(); j++) {
-				int next = adj[tmp][j];
-				if (!visited[next]) {
-					visited[next] = 1;
-					q.push(next);
-				}
-			}
-		}
-		depth++;
-	}
-	return -1;
+      for (int next : adj[cur]) {
+        if (!v[next]) {
+          v[next] = 1;
+          q.push(next);
+        }
+      }
+    }
+    ans++;
+  }
+  return -1;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> n;
-	cin >> p1 >> p2;
-	cin >> m;
-	adj = vector<vector<int>>(n + 1, vector<int>());
-	visited = vector<int>(n + 1, 0);
-	for (int i = 0; i < m; i++) {
-		int tmp1, tmp2;
-		cin >> tmp1 >> tmp2;
-		adj[tmp1].push_back(tmp2);
-		adj[tmp2].push_back(tmp1);
-	}	
-	cout << bfs() << '\n';
-	return 0;
+  cin >> n;
+  adj = vector<vector<int> >(n + 1);
+  cin >> x >> y;
+  cin >> m;
+  for (int i = 0; i < m; i++) {
+    int a, b;
+    cin >> a >> b;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+  cout << bfs() << '\n';
+  return 0;
 }
