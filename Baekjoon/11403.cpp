eef7af789b72cbe2fd244f4bd2cdfa05ba489cdd
mbkim95 <mbkim95@naver.com>
2021-07-10 23:18:01 +0900

    Floyd-Warshall

--- Baekjoon/11403.cpp
+++ Baekjoon/11403.cpp
@@ -1,45 +1,22 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int n;
-vector<vector<int>> adj, ans;
-vector<int> visited;
-
-void dfs(int idx) {
-	for (int i = 1; i <= n; i++) {
-		if (adj[idx][i] && !visited[i]) {
-			visited[i] = 1;
-			dfs(i);
-		}
-	}
-}
+int n, adj[101][101];
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cin >> n;
-	adj = vector<vector<int>>(n + 1, vector<int>(n + 1, 0));
-	ans = vector<vector<int>>(n + 1, vector<int>(n + 1, 0));
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			cin >> adj[i][j];
-		}
-	}
+  cin >> n;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      cin >> adj[i][j];
 
-	for (int i = 1; i <= n; i++) {
-		visited = vector<int>(n + 1, 0);
-		dfs(i);
-		for (int j = 1; j <= n; j++) {
-			if (visited[j])
-				ans[i][j] = 1;
-		}
-	}
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			cout << ans[i][j] << ' ';
-		}
-		cout << '\n';
-	}
-	return 0;
+  for (int k = 0; k < n; k++)
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < n; j++)
+        if (adj[i][k] & adj[k][j]) adj[i][j] = 1;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) cout << adj[i][j] << ' ';
+    cout << '\n';
+  }
+  return 0;
 }
