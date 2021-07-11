7e335f04ddb5738243d7f51f08e2cda526040a52
mbkim95 <mbkim95@naver.com>
2021-07-12 01:16:51 +0900

    Dynamic Programming

--- Baekjoon/1309.cpp
+++ Baekjoon/1309.cpp
@@ -1,30 +1,17 @@
 #include <iostream>
-#include <cstring>
+#define MOD 9901
 using namespace std;
 
-int n;
-int dp[100001];
-
-int solve(int idx) {
-	int& ret = dp[idx];
-	if (ret != -1) return ret;
-
-	if (idx == 1)
-		return ret = 3;
-	
-	if (idx == 2)
-		return ret = 7;
-
-	return ret = (2 * solve(idx - 1) + solve(idx - 2)) % 9901;
-}
+int n, dp[100001][3];
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> n;
-	memset(dp, -1, sizeof(dp));
-	cout << solve(n) << '\n';
-	return 0;
+  cin >> n;
+  dp[0][2] = 1;
+  for(int i=1; i<=n; i++) {
+    dp[i][0] = (dp[i-1][1] + dp[i-1][2]) % MOD;
+    dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % MOD;
+    dp[i][2] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % MOD;
+  }
+  cout << (dp[n][0] + dp[n][1] + dp[n][2]) % MOD << '\n';
+  return 0;
 }
