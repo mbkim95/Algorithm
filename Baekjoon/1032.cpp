cdf2f4316c324cc62b4bca654682addc76369efa
mbkim95 <mbkim95@naver.com>
2021-08-02 22:35:27 +0900

    Implementation

--- Baekjoon/1032.cpp
+++ Baekjoon/1032.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int n;
-	cin >> n;
-	vector<string> str(n);
-	for (int i = 0; i < n; i++)
-		cin >> str[i];
-	string ans = str[0];
-	for (int i = 1; i < n; i++) 
-		for (int j = 0; j < ans.length(); j++) 
-			if (ans[j] != str[i][j]) {
-				ans[j] = '?';
-	cout << ans << '\n';
-	return 0;
+  int n;
+  cin >> n;
+  vector<string> str(n);
+  for (int i = 0; i < n; i++) cin >> str[i];
+  string ans = str[0];
+  for (int i = 1; i < n; i++)
+    for (int j = 0; j < ans.length(); j++)
+      if (ans[j] != str[i][j]) ans[j] = '?';
+  cout << ans << '\n';
+  return 0;
 }
