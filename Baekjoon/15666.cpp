5f7a38d85f9c207aac942592c86d59212374541b
mbkim95 <mbkim95@naver.com>
2021-08-07 21:13:45 +0900

    DFS

--- Baekjoon/15666.cpp
+++ Baekjoon/15666.cpp
@@ -1,44 +1,39 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <unordered_set>
+#include <set>
+#include <vector>
 using namespace std;
 
 int n, m;
-vector<int> num, result;
-unordered_set<string> s;
+vector<int> num, ans;
+set<string> s;
 
-void dfs(int idx){
-    if(result.size() == m){
-        string str = "";
-        for(int i : result){
-            str += to_string(i);
-            str += ' ';
-        }
-        if(s.find(str) == s.end()){
-            s.insert(str);
-            cout << str << '\n';
-        }
-        return;
+void dfs(int idx, int cnt) {
+  if (cnt == m) {
+    string str = "";
+    for (int i : ans) str += to_string(i) + " ";
+    if (s.find(str) == s.end()) {
+      s.insert(str);
+      cout << str << '\n';
     }
+    return;
+  }
 
-    for(int i=idx; i<n; i++){
-        result.push_back(num[i]);
-        dfs(i);
-        result.pop_back();
-    }
+  for (int i = idx; i < n; i++) {
+    ans.push_back(num[i]);
+    dfs(i, cnt + 1);
+    ans.pop_back();
+  }
 }
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> n >> m;
-    num.resize(n);
-    for(int i=0; i<n; i++)
-        cin >> num[i];
-
-    sort(num.begin(), num.end());
-    dfs(0);
-    return 0;
-}15 
+int main() {
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    int tmp;
+    cin >> tmp;
+    num.push_back(tmp);
+  }
+  sort(num.begin(), num.end());
+  dfs(0, 0);
+  return 0;
+}
