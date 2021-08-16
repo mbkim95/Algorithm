3eabe32d0215b3d613dc9cd7e55dc9dcb9e78a82
mbkim95 <mbkim95@naver.com>
2021-08-16 13:29:54 +0900

    Implementation

---
+++ Leetcode/MergeTwoSortedLists.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from typing import List
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if (not l1) or (l2 and l1.val > l2.val):
+            l1, l2 = l2, l1
+        if l1:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+        return l1
+
+
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(4)
+l2 = ListNode(1)
+l2.next = ListNode(3)
+l2.next.next = ListNode(4)
+answer = Solution().mergeTwoLists(l1, l2)
+while answer:
+    print(answer.val, end=' ')
+    answer = answer.next
