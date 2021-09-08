ec74c00d56b48dac55192f278d73be4fcfbe82b2
mbkim95 <mbkim95@naver.com>
2021-09-08 23:22:16 +0900

    Implementation

--- Baekjoon/15654.cpp
+++ Baekjoon/15654.cpp
@@ -1,33 +1,33 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-int n, m, num[8], chk[8];
-vector<int> ans;
-
-void dfs(int cur) {
-  if(ans.size() == m){
-    for(int i : ans) cout << i << ' ';
+void dfs(int n, int m, vector<int>& v, vector<int>& ans) {
+  if (ans.size() == m) {
+    for (int i : ans) cout << i << ' ';
     cout << '\n';
     return;
   }
 
-  for(int i=0; i < n; i++){
-    if(!chk[i]){
-      chk[i] = 1;
-      ans.push_back(num[i]);
-      dfs(i);
-      ans.pop_back();
-      chk[i] = 0;
-    }
+  for (int i = 0; i < n; i++) {
+    ans.push_back(v[i]);
+    dfs(n, m, v, ans);
+    ans.pop_back();
   }
 }
 
 int main() {
+  int n, m;
   cin >> n >> m;
-  for (int i = 0; i < n; i++) cin >> num[i];
-  sort(num, num + n);
-  dfs(0);
+  vector<int> v(n);
+  vector<int> ans;
+  for (int i = 0; i < n; i++) cin >> v[i];
+  sort(v.begin(), v.end());
+  for (int i = 0; i < n; i++) {
+    ans.push_back(v[i]);
+    dfs(n, m, v, ans);
+    ans.pop_back();
+  }
   return 0;
 }
