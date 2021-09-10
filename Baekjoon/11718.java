6fb87035fd9069951960dfbb65f31b508877cd95
mbkim95 <mbkim95@naver.com>
2021-09-10 23:35:53 +0900

    Implementation

---
+++ Baekjoon/11718.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNextLine()) {
+            System.out.println(scan.nextLine());
+        }
+    }
+}
