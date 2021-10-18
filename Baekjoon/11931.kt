71019d9b4169ee4fa947f97c8cad632bb3246f2b
mbkim95 <mbkim95@naver.com>
2021-10-18 22:31:07 +0900

    Implementation

--- Baekjoon/11931.kt
+++ Baekjoon/11931.kt
@@ -1,14 +1,12 @@
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
+import java.io.*
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
     val nums = arrayListOf<Int>()
-    for (i in 1..br.readLine()!!.toInt())
+    repeat(br.readLine()!!.toInt()) {
         nums.add(br.readLine()!!.toInt())
+    }
     nums.sortDescending()
     for (i in nums) bw.write("$i\n")
     bw.flush()
