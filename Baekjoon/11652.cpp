265fe06692a0874063cec362ad74d32f3ad7d63e
mbkim95 <mbkim95@naver.com>
2022-03-04 23:05:43 +0900

    Implementation

--- Baekjoon/11652.cpp
+++ Baekjoon/11652.cpp
@@ -1,42 +1,28 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <map>
+#include <climits>
 using namespace std;
 
 int n;
-long long ans;
-vector<long long> v;
-
-void solve() {
-	sort(v.begin(), v.end());
-	v.push_back(0);
-	ans = v[0];
-	int cnt = 1;
-	int maxCnt = 1;
-	for (int i = 1; i <= n; i++) {
-		if (v[i] != v[i - 1]){
-			if (maxCnt < cnt) {
-				maxCnt = cnt;
-				ans = v[i - 1];
-			}
-			cnt = 1;
-		}
-		else 
-			cnt++;
-	}
-}
+map<long long, int> m;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		long long tmp;
-		cin >> tmp;
-		v.push_back(tmp);
-	}
-	solve();
-	cout << ans << '\n';
-	return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    cin >> n;
+    int cnt = 0;
+    long long ans = LLONG_MIN;
+    while(n--) {
+        long long tmp;
+        cin >> tmp;
+        m[tmp]++;
+        if(cnt < m[tmp]) {
+            cnt = m[tmp];
+            ans = tmp;
+        } else if(cnt == m[tmp] && ans > tmp) 
+            ans = tmp;
+    }
+    cout << ans << '\n';
+    return 0;
 }
