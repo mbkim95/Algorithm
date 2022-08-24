e599b20951e13193d4c7b8a2206602f39cb49ef8
mbkim95 <mbkim95@naver.com>
2022-08-24 20:46:54 +0900

    Implementation

--- Leetcode/reorder_data_in_log_files.py
+++ Leetcode/reorder_data_in_log_files.py
@@ -10,9 +10,9 @@ class Solution:
             else:
                 letters.append(log)
 
+        # 2개의 키를 람다 표현식으로 정렬
         letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
         return letters + digits
 
-
-logs = ["a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"]
+logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
 print(Solution().reorderLogFiles(logs))
