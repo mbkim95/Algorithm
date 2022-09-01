80dbdef2295a8d61043106f33971b63a2eccc0a6
mbkim95 <mbkim95@naver.com>
2022-09-01 21:43:43 +0900

    Implementation

--- Baekjoon/2566.cpp
+++ Baekjoon/2566.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int n, f;
-
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
-	int row, col;
-	int max_value = 0;
+    int r = 1, c = 1, tmp, max_value = 0;
 
-	for (int i = 1; i <= 9; i++) {
-		for (int j = 1; j <= 9; j++) {
-			int tmp;
-			cin >> tmp;
-			if (max_value < tmp) {
-				max_value = tmp;
-				row = i;
-				col = j;
-			}
-		}
-	}
-	cout << max_value << '\n' << row << ' ' << col << '\n';
-	return 0;
+    for(int i=1; i<=9; i++) {
+        for(int j=1; j<=9; j++) {
+            cin >> tmp;
+            if(tmp > max_value) {
+                max_value = tmp;
+                r = i;
+                c = j;
+            }
+        }
+    }
+    cout << max_value << '\n' << r << ' ' << c << '\n';
+    return 0;
 }
