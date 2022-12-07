342f18ff36a216f08c273e906f047de1fd53e58f
mbkim95 <mbkim95@naver.com>
2022-12-07 10:34:05 +0900

    Implementation

---
+++ Baekjoon/20254.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int ur, tr, uo, to;
+    cin >> ur >> tr >> uo >> to;
+    cout << 56 * ur + 24 * tr + 14 * uo + 6 * to << '\n';
+    return 0;
+}
