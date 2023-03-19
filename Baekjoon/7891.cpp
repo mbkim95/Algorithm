d00c820a8e8caef55d531668f984b07738c0e4b5
mbkim95 <mbkim95@naver.com>
2023-03-19 21:42:04 +0900

    Implementation

---
+++ Baekjoon/7891.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    while(n--) {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+    return 0;
+}
